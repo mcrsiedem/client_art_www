830648a3e928e3d652ea9544c5c3697d74e2298c
Maciek <maciek@artdruk.eu>
2023-05-17 16:28:18 +0200

    dodany status falcowanie do filtrowania maszyn

---
+++ src/components/DrukPlan/Row.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+function Row(){
+
+    const [title,setTitle]= useState('');
+
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Row;

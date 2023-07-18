d7cd0c7dfe53a463c2ea8aeabf0088262c87247b
Maciek <maciek@artdruk.eu>
2023-07-18 15:18:19 +0200

    login

--- src/artapp/Login.js
+++ src/artapp/Login.js
@@ -2,27 +2,47 @@ import React from "react";
 import { useState } from "react";
 import style from '../artapp/Login.modules.css';
 import 'bootstrap/dist/css/bootstrap.css'
+import axios from "axios";
+import {ip} from "../Host";
 
 
 function Login(){
 
+    const [values, setValues] = useState({
+        login: '',
+        haslo:''
+    })
 
+    const [user,setUser] = useState([]);
+
+
+   const handleSubmit = (event) =>{
+event.preventDefault();
+const r =axios.get(ip + 'users/'+values.login+'/'+values.haslo).then(res => console.log(res) );
+
+console.log(r.data[0].imie);
+
+    }
         return (
-            <div className='d-flex justify-content-center align-items-center vh-100'>
+            <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
 
                 <div className='bg-white p-3 rounded w-25 border'>
 
-                    <h2>Zaloguj</h2>
+                    <h2>Zaloguj</h2> 
 
-                    <form>
+                    <form onSubmit={handleSubmit} >
                         <div className='mb-3'>
                             <label for="login">Login:</label>
-                            <input type="text" name='login' className="from-control rounded-0" />
+                            <input type="text" name='login' 
+                            onChange={e => setValues({...values, login: e.target.value})}
+                            className="from-control rounded-0" />
                         </div>
 
                         <div className='mb-3'>
                             <label for="password">Hasło:</label>
-                            <input type="password" name='haslo' className="from-control rounded-0" />
+                            <input type="password" name='haslo' 
+                            onChange={e => setValues({...values, haslo: e.target.value})}
+                            className="from-control rounded-0" />
                         </div>
 
                         <button type='submit' className='btn btn-success w-100 rounded-0'> Zaloguj</button>

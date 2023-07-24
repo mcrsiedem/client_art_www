1685230bc8b46f9a3aa960968896e05930efbc18
Maciek <maciek@artdruk.eu>
2023-07-24 14:38:44 +0200

    m

--- src/Host.js
+++ src/Host.js
@@ -1,7 +1,7 @@
- const ip = 'http://46.41.151.63:3001/api/';
+ //const ip = 'http://46.41.151.63:3001/api/';
  //const ip = 'http://localhost:3001/api/';
 
-// const ip = 'http://localhost:3002/api_www/';
+ const ip = 'http://localhost:3002/api_www/';
 
 export  {
     ip

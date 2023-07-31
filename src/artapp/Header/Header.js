58d24ee38dd217dd411332949ca62bf4b4443c68
Maciek <maciek@artdruk.eu>
2023-07-31 15:59:08 +0200

    black

--- src/artapp/Header/Header.js
+++ src/artapp/Header/Header.js
@@ -2,17 +2,28 @@ import React from "react";
 import style from './Header.module.css';
 import Searchbar from '../Searchbar';
 import ReactLogo from './grid.svg';
+import ReactLogo_ilosc from './ilosc.svg';
+import ReactLogo_ustawienia from './settings.svg';
 
 function Header(){
     return (
         <header id='header' className={style.body}>
-            <img className={style.grid} src={ReactLogo} alt="React Logo" />
-            <img className={style.grid} src={ReactLogo} alt="React Logo" />
-            <img className={style.grid} src={ReactLogo} alt="React Logo" />
-            <img className={style.grid} src={ReactLogo} alt="React Logo" />
-            <img className={style.grid} src={ReactLogo} alt="React Logo" />
-                {/* <Searchbar/>
-                <p>1</p> */}
+            <div className={style.leftHeaderContener}>
+                <img className={style.icon} src={ReactLogo_ustawienia} alt="React Logo" />
+                <img className={style.icon} src={ReactLogo} alt="React Logo" />
+                <img className={style.icon} src={ReactLogo} alt="React Logo" />
+                <img className={style.icon} src={ReactLogo} alt="React Logo" />
+                <img className={style.icon} src={ReactLogo_ilosc} alt="React Logo" />
+            </div >
+            <div className={style.leftHeaderContener}>
+
+            </div>
+            <div className={style.rightHeaderContener}>
+                <Searchbar />
+            </div>
+            
+             
+               
         </header>
     );
 }

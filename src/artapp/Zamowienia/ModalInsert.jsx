56b861009f39c59bab0871ac3727b615e28a7c64
Maciek <maciek@artdruk.eu>
2023-10-13 12:35:28 +0200

    svg

--- src/artapp/Zamowienia/ModalInsert.jsx
+++ src/artapp/Zamowienia/ModalInsert.jsx
@@ -4,11 +4,24 @@ import style from '../Zamowienia/ModalInsert.module.css';
 function ModalInsert({ openModalInsert, setOpenModalInsert }) {
     return (
         <>
-            <div className={style.mod}>
-                <div>
-                   <button onClick={() => setOpenModalInsert(false)}> X </button> 
+            <div className={style.container}>
+                <div className={style.header}>
+                <button className={style.buttonX} onClick={() => setOpenModalInsert(false)}> X </button> 
+                
                 </div>
-                <div> me</div>
+
+                <div className={style.center}>
+                    <input type="text" />
+                    <input type="text" />
+                    <p>center</p>
+                </div>
+
+
+                <div className={style.footer}>
+                <p>footer</p>
+                </div>
+                
+                
                 {/* <div>id {row.id}</div>
                 <div>id + user {row.user}</div> */}
                 

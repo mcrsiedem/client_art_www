099989182bd6be478d0a7722c5fc1a0f99861357
Maciek <maciek@artdruk.eu>
2023-10-31 12:06:12 +0100

    dane

--- src/artapp/Zamowienia/ModalInsert/components/Elementy.jsx
+++ src/artapp/Zamowienia/ModalInsert/components/Elementy.jsx
@@ -16,15 +16,34 @@ function ElementKafel({ element }) {
   return (
     <div className={style.elementKafel}>
 
-    <div className={style.header}>
-    {element.typ} 
-    </div>
-    <div className={style.center}>
-    <div className={style.col}>
-              <label className={style.label}> Typ</label>
-              <input className={style.tytul} defaultValue={element.typ}></input>
-            </div>
-    </div>
+      <div className={style.header}>
+        {element.typ} 1000 szt.
+      </div>
+      <div className={style.center}>
+        <div className={style.col}>
+          <label className={style.label}> Wersja</label>
+          <input className={style.tytul} defaultValue={element.nazwa}></input>
+        </div>
+        <div className={style.col}>
+          <label className={style.label}> Strony</label>
+          <input className={style.tytul} defaultValue={element.ilosc_stron}></input>
+        </div>
+        <div className={style.col}>
+          <label className={style.label}> Format</label>
+          <input className={style.tytul} defaultValue={element.format_x}></input>
+          <input className={style.tytul} defaultValue={element.format_y}></input>
+        </div>
+        <div className={style.col}>
+          <label className={style.label}> Kolory</label>
+          <input className={style.tytul} defaultValue={element.kolor_front}></input>
+          <input className={style.tytul} defaultValue={element.kolor_back}></input>
+        </div>
+        <div className={style.col}>
+          <label className={style.label}> Papier</label>
+          <input className={style.tytul} defaultValue={element.kolor_front}></input>
+          <input className={style.tytul} defaultValue={element.kolor_back}></input>
+        </div>
+      </div>
 
 
     </div>

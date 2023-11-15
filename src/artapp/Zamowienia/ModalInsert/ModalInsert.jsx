283ce864ee675a8036d5cb4280f2aaff5bb9cd6e
Maciek <maciek@artdruk.eu>
2023-11-15 15:28:46 +0100

    nie wiem

--- src/artapp/Zamowienia/ModalInsert/ModalInsert.jsx
+++ src/artapp/Zamowienia/ModalInsert/ModalInsert.jsx
@@ -38,52 +38,63 @@ const [idZamowienie, setIdZamowienia] = useState();
 
     const zamowienie_id = res.data.insertId
     setIdZamowienia(zamowienie_id);
-    setProdukty(produkty.map((t)=>({...t, zamowienie_id: zamowienie_id})));
+
+    // let res2 = await axios.post(ip + 'produkty', { tytul: produkt.tytul, zamowienie_id: zamowienie_id });
+    //    let produkt_id = res.data.insertId;
+    // setProdukty(produkty.map((t,i)=>({...t,id: async()=> {
+    //   let res2 = await axios.post(ip + 'produkty', { tytul: t.tytul, zamowienie_id: zamowienie_id });
+    //       let produkt_id = res2.data.insertId;
+    //    if (t.index=== i){return produkt_id} else{ return t.id}
+      
+      
+      
+    //   }, zamowienie_id: zamowienie_id})));
 
 
 
    
-  // setProdukty(produkty.map(async (produkt,i) => {
-  //     let res = await axios.post(ip + 'produkty', { tytul: produkt.tytul, zamowienie_id: zamowienie_id });
-  //     let produkt_id = res.data.insertId;
+  const nextProdukt = produkty.map(async (produkt,i) => {
+      let res = await axios.post(ip + 'produkty', { tytul: produkt.tytul, zamowienie_id: zamowienie_id });
+      let produkt_id = res.data.insertId;
 
-  //     console.log(produkt)
-  //     if(produkt.index ===i){
-  //       return { ...produkt, id: produkt_id, zamowienie_id: zamowienie_id };
-  //     }
-  //     else{
-  //       return produkt
-  //     }
+      console.log(nextProdukt)
+      // if(produkt.index ===i){
+      //   return { ...produkt, id: produkt_id, zamowienie_id: zamowienie_id };
+      // }
+      // else{
+      //   return produkt
+      // }
 
 
-  //       // if (produkt.index === i) {
-  //       //    console.log(produkt.index);
-  //       //    console.log(i);
-  //       //   return { ...produkt, id: produkt_id, zamowienie_id: zamowienie_id };
-  //       // } else {
-  //       //   return produkt;;
-  //       // }
+        // if (produkt.index === i) {
+        //    console.log(produkt.index);
+        //    console.log(i);
+        //   return { ...produkt, id: produkt_id, zamowienie_id: zamowienie_id };
+        // } else {
+        //   return produkt;;
+        // }
       
-  //     // setProdukty(
-  //     //   produkty.map((t) => {
-  //     //     if (t.index === i) {
-  //     //       console.log(t.index);
-  //     //       return { ...t, id: produkt_id, zamowienie_id: zamowienie_id };
-  //     //     } else {
-  //     //       return t;
-  //     //     }
-  //     //   })
-  //     // );
+      // setProdukty(
+      //   produkty.map((t) => {
+      //     if (t.index === i) {
+      //       console.log(t.index);
+      //       return { ...t, id: produkt_id, zamowienie_id: zamowienie_id };
+      //     } else {
+      //       return t;
+      //     }
+      //   })
+      // );
 
       
-  //    }
+     }
 
+    
 
-  //   )
+    )
 
 
  
-  // )
+  
 
 
   }

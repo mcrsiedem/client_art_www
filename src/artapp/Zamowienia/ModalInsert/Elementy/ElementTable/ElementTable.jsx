fe8e6e9f2264d51ba002aec75b4a8b49f6a16754
Maciek <maciek@artdruk.eu>
2023-11-30 15:02:12 +0100

    elementy

--- src/artapp/Zamowienia/ModalInsert/Elementy/ElementTable/ElementTable.jsx
+++ src/artapp/Zamowienia/ModalInsert/Elementy/ElementTable/ElementTable.jsx
@@ -7,14 +7,7 @@ import ElementTableHeader from "./ElementTableHeader";
 
 
 export default function ElementTable({elementy,setElementy,handleChangeCardElementy,selected_papier,setSelected_papier,fragmenty,setFragmenty,info,setInfo,listaWykonczenia}) {
-  //  const nakladHandler = (e,row) => { handleChangeCardElementy({ ...row, naklad: e.target.value }) }
-  // const nazwaHandler = (e) => { handleChangeCardElementy({ ...card, nazwa: e.target.value }) }
-  // const stronyHandler = (e) => { handleChangeCardElementy({ ...card, ilosc_stron: e.target.value }) }
-  // const formatXHandler = (e) => { handleChangeCardElementy({ ...card, format_x: e.target.value }) }
-  // const formatYHandler = (e) => { handleChangeCardElementy({ ...card, format_y: e.target.value }) }
-  // const kolorFrontHandler = (e) => { handleChangeCardElementy({ ...card, kolor_front: e.target.value }) }
-  // const kolorBackHandler = (e) => { handleChangeCardElementy({ ...card, kolor_back: e.target.value }) }
-  // const papierkHandler = (e) => { handleChangeCardElementy({ ...card, papier_id: e.target.value }) }
+
   return (
     <div className={style.elementCard}>
         
@@ -31,19 +24,20 @@ export default function ElementTable({elementy,setElementy,handleChangeCardEleme
 <table className={style.table}>
 <thead>
           <tr >
-            <th className={style.col1}>Zam.</th>
+            {/* <th className={style.col1}>Zam.</th>
             <th className={style.col2}>Prod.</th>
-            <th className={style.col3}>#</th>
+            <th className={style.col3}>#</th> */}
+            <th className={style.col_id}>#</th>
             <th className={style.col_typ}>Typ</th>
             <th className={style.col_naklad}>Nakład</th>
-            <th className={style.col_nazwa}>Nazwa</th>
+            <th className={style.col_wersja}>Wersja</th>
             <th className={style.col_strony}>Strony</th>
             <th className={style.col_kolory}>Kolory</th>
             <th className={style.col_format} colspan="2">Netto</th>
            
             <th className={style.col7}>Papier</th>
-            <th className={style.col7}>Gramatura</th>
-            <th className={style.col7}>Wykończenie</th>
+            <th className={style.col_gramatura}>Gramatura</th>
+            <th className={style.col_wykonczenie}>Wykończenie</th>
             <th className={style.col7}>Lakier</th>
             <th className={style.col7}>OK</th>
           </tr>
@@ -51,7 +45,7 @@ export default function ElementTable({elementy,setElementy,handleChangeCardEleme
         </thead>
         <tbody>
 
-        {elementy.map((row) => {
+        {elementy.map((row,i) => {
                                 return (
                                     <tr
                                     key={row.id}
@@ -61,18 +55,20 @@ export default function ElementTable({elementy,setElementy,handleChangeCardEleme
                                     //     setRow({ id: row.id, user: row.user });
                                     // }}
                                     >
-                                            <td>{row.zamowienie_id}</td>
+                                            {/* <td>{row.zamowienie_id}</td>
                                             <td>{row.produkt_id}</td>
-                                            <td>{row.id}</td>
+                                            <td>{row.id}</td> */}
+                                            <td>{i+1}</td>
                                             <td>{row.typ}</td>
                                             <td><input defaultValue={row.naklad} onChange={(e)=>handleChangeCardElementy({ ...row, naklad: e.target.value })}></input></td>
                                             <td><input defaultValue={row.nazwa} onChange={(e)=>handleChangeCardElementy({ ...row, nazwa: e.target.value })}></input></td>
-                                            <td>{row.ilosc_stron}</td>
-                                            <td>{row.kolor_front}</td>
-                                            <td>{row.format_x}</td>
-                                            <td>{row.format_y}</td>
-                                            <td>{row.papier_id}</td>
-                                            <td>{row.gramatura}</td>
+                                            <td><input defaultValue={row.ilosc_stron} onChange={(e)=>handleChangeCardElementy({ ...row, ilosc_stron: e.target.value })}></input></td>
+                                            <td><input defaultValue={row.kolory} onChange={(e)=>handleChangeCardElementy({ ...row, kolory: e.target.value })}></input></td>
+                                            <td className={style.col_format}><input defaultValue={row.format_x} onChange={(e)=>handleChangeCardElementy({ ...row, format_x: e.target.value })}></input></td>
+                                            <td className={style.col_format}><input defaultValue={row.format_y} onChange={(e)=>handleChangeCardElementy({ ...row, format_y: e.target.value })}></input></td>
+                                            <td><input defaultValue={row.papier_id} onChange={(e)=>handleChangeCardElementy({ ...row, papier_id: e.target.value })}></input></td>
+                                            <td><input defaultValue={row.gramatura} onChange={(e)=>handleChangeCardElementy({ ...row, gramatura: e.target.value })}></input></td>
+                                         
                                             <td><select
                                                 className={style.select}
                                                 defaultValue={row.wykonczenie}

a61815ce42ecde1464ac443f57452927172f4a48
Maciek <maciek@artdruk.eu>
2023-12-11 10:45:38 +0100

    sortowanie

--- src/eLesson/Sortowanie.jsx
+++ src/eLesson/Sortowanie.jsx
@@ -1,5 +1,13 @@
+
+// sortowwanie 1,2,3
 {listaDostepnychGramatur.sort((a,c)=>a.gramatura-c.gramatura).map((option) => (
     <option key={option.id} value={option.id}>
       {option.gramatura} g/m2 vol. {option.bulk}  {option.wykonczenie}
     </option>
   ))}
+
+
+// po jedyn z kazdego elementu
+listaDostepnychGramatur.map((el)=> el.wykonczenie)
+.filter((currentValue, index, arr) => (
+   arr.indexOf(currentValue) === index))

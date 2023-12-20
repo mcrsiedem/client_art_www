3aa269a71bd2ae3c5d3c68ff34959abfb767b6d7
Maciek <maciek@artdruk.eu>
2023-12-20 11:33:00 +0100

    procesy table

--- src/artapp/Zamowienia/ModalInsert/Elementy/ElementyProcesInsert.jsx
+++ src/artapp/Zamowienia/ModalInsert/Elementy/ElementyProcesInsert.jsx
@@ -14,7 +14,52 @@ export default function ElementyProcesInsert({showElementyProcesyInsert,setShowE
     <div className={style.insertContainer}>
       
       <div className={style.header} > <p className={style.title}>Procesy</p></div>
-      <div className={style.center} > </div>
+      <div className={style.center} > 
+      <table className={style.table2}>
+        <thead>
+          <tr>
+            <th className={style.col1}>#</th>
+            <th className={style.col_typ}>Proces</th>
+            <th className={style.col_naklad}>Strona</th>
+            <th className={style.col_wersja}>Nazwa</th>
+            <th className={style.col_strony}>Info</th>
+
+            <th className={style.col_kolory}>Dodatkowe informacje </th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr >
+      <td>1</td>
+      <td>1</td>
+      <td>1</td>
+      <td>1</td>
+      <td>1</td>
+      <td>1</td>
+
+
+
+      </tr>
+          {/* {elementy.map((row, i) => {
+            return (
+              <RowElement
+                i={i}
+                row={row}
+                handleChangeCardElementy={handleChangeCardElementy}
+                listaPapierow={listaPapierow}
+                listaGramatur={listaGramatur}
+                setListaGramatur={setListaGramatur}
+                isEdit={isEdit}
+                setIsEdit={setIsEdit}
+                procesyElementow={procesyElementow}
+                setProcesyElementow={setProcesyElementow}
+                listaDostepnychProcesow={listaDostepnychProcesow}
+                setShowElementyProcesyInsert={setShowElementyProcesyInsert}
+              />
+            );
+          })} */}
+        </tbody>
+      </table>
+      </div>
       <div className={style.row} > <button className={style.btn} onClick={() => { setShowElementyProcesyInsert(false) }} >Anuluj</button > <button className={style.btn}>OK</button></div>
     </div>
   );

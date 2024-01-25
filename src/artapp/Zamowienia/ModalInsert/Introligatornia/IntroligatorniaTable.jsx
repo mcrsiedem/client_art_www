3b75b7e7cb2547ee92bc3040a67cf0a22da057a1
Maciek <maciek@artdruk.eu>
2024-01-25 11:58:39 +0100

    oprawa

--- src/artapp/Zamowienia/ModalInsert/Introligatornia/IntroligatorniaTable.jsx
+++ src/artapp/Zamowienia/ModalInsert/Introligatornia/IntroligatorniaTable.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+
 import style from "./IntroligatorniaTable.module.css";
 import logoExpand from "../../../../svg/expand.svg";
 import { _rodzaj_oprawy } from "../api";
@@ -13,94 +13,88 @@ export default function IntroligatorniaTable({
 }) {
 
   const [expand,setExpand] =useState(true);
+
   return (
-    <>
-      <div className={style.oprawy}>
-        <Oprawa handleChangeCardOprawa={handleChangeCardOprawa} oprawa={oprawa} setOprawa={setOprawa}fragmenty={fragmenty} expand={expand} setExpand={setExpand}>
-          {" "}
-        </Oprawa>
-        {/* {zestawy.map((prod) => (
+    <div className={style.container}>
+      <div className={style.oprawa}>
+         <Header  />
+      <OprawaTable oprawa={oprawa} handleChangeCardOprawa={handleChangeCardOprawa} fragmenty={fragmenty} expand={expand} setExpand={setExpand}/>
+</div>
+     
 
-        
-          ))}
-        <Oprawa nr={1000}/> */}
-      </div>
-    </>
-  );
-}
-
-function Oprawa({ nr, oprawa, setOprawa,fragmenty,expand,setExpand,handleChangeCardOprawa }) {
-  return (
-    <div className={style.oprawaTable}>
-      <Header nr={nr} />
-
-      <div className={style.main}>
-        <table className={style.table}>
-          <thead>
-            <tr>
-              <th className={style.col1}>Zam.</th>
-              <th className={style.col2}>Prod.</th>
-              <th className={style.col3}>#</th>
-              <th className={style.col4}>Oprawa</th>
-              <th className={style.col5}>Nakład</th>
-              <th className={style.col6}>Data spedycji</th>
-              <th className={style.col7}>Uwagi</th>
-              <th className={style.col7}>Fragmenty</th>
-            
-
-            </tr>
-          </thead>
-          <tbody>
-            {oprawa.map((row) => {
-              return (
-                <>
-                <tr
-                  key={row.id}
-                >
-                  <td>{row.zamowienie_id}</td>
-                  <td>{row.produkt_id}</td>
-                  <td>{row.id}</td>
-                  <RodzajOprawy row={row} handleChangeCardOprawa={handleChangeCardOprawa}/>
-                  <td>{row.naklad}</td>
-                  <DataSpedycji row={row} handleChangeCardOprawa={handleChangeCardOprawa}/>
-                  {/* <td>{row.data_spedycji}</td> */}
-                
-                  <td>{row.uwagi}</td>
-                  <img
-          className={style.icon}
-          src={logoExpand}
-          onClick={() => {setExpand(!expand)}}
-          alt="Procesy"
-        />
-
-</tr>
-                  {expand ? fragmenty
-                  .filter((el) => el.oprawa_id === row.id)
-                  .map((row) => {
-                    return <tr key={row.id}>
-                      <td></td><td></td><td></td>
-                      <td>
-                      {row.typ} 
-                      </td>
-                      <td>
-                      {row.naklad}
-                      </td>
-                      <td></td><td></td><td></td>
-                     </tr>;
-                  }):<></>}
-                </>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
     </div>
   );
 }
 
+function OprawaTable({oprawa,handleChangeCardOprawa, fragmenty, expand, setExpand}){
+  return (  <div className={style.main}>
+  <table className={style.table}>
+    <thead>
+      <tr>
+        <th className={style.col1}>Zam.</th>
+        <th className={style.col2}>Prod.</th>
+        <th className={style.col3}>#</th>
+        <th className={style.col4}>Oprawa</th>
+        <th className={style.col5}>Nakład</th>
+        <th className={style.col6}>Data spedycji</th>
+        <th className={style.col7}>Uwagi</th>
+        <th className={style.col7}>Fragmenty</th>
+      
 
-function Header({ nr }) {
-  return <div className={style.header}>Oprawa {nr}</div>;
+      </tr>
+    </thead>
+    <tbody>
+      {oprawa.map((row) => {
+        return (
+
+
+          
+          <>
+          <tr
+            key={row.id}
+          >
+            <td>{row.zamowienie_id}</td>
+            <td>{row.produkt_id}</td>
+            <td>{row.id}</td>
+            <RodzajOprawy row={row} handleChangeCardOprawa={handleChangeCardOprawa}/>
+            <td>{row.naklad}</td>
+            <DataSpedycji row={row} handleChangeCardOprawa={handleChangeCardOprawa}/>
+            {/* <td>{row.data_spedycji}</td> */}
+          
+            <td>{row.uwagi}</td>
+            <img
+    className={style.icon}
+    src={logoExpand}
+    onClick={() => {setExpand(!expand)}}
+    alt="Procesy"
+  />
+
+          </tr>
+            {expand ? fragmenty
+            .filter((el) => el.oprawa_id === row.id)
+            .map((row) => {
+              return <tr key={row.id}>
+                <td></td><td></td><td></td>
+                <td>
+                {row.typ} 
+                </td>
+                <td>
+                {row.naklad}
+                </td>
+                <td></td><td></td><td></td>
+          </tr>;
+            }):<></>}
+          </>
+        );
+      })}
+    </tbody>
+  </table>
+</div>)
+
+}
+
+function Header() {
+  return <div className={style.header}></div>;
 }
 
 function DataSpedycji({row,handleChangeCardOprawa}){

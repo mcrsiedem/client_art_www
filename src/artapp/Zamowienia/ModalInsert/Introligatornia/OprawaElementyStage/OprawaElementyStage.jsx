a68d09668cf473a5204702b846142c3e91b23117
Maciek <maciek@artdruk.eu>
2024-02-05 16:00:07 +0100

    fraG

--- src/artapp/Zamowienia/ModalInsert/Introligatornia/OprawaElementyStage/OprawaElementyStage.jsx
+++ src/artapp/Zamowienia/ModalInsert/Introligatornia/OprawaElementyStage/OprawaElementyStage.jsx
@@ -7,9 +7,7 @@ import style from "./OprawaElementyStage.module.css";
 // import { useState } from "react";
 // import Logo_ustawienia from "../../../../svg/settings.svg";
 
-
 // okienko do wydzielania części z oprawy
-
 
 export default function OprawaElementyStage({
   setShowOprawaElementyStage,
@@ -18,147 +16,93 @@ export default function OprawaElementyStage({
   oprawa,
   setOprawa,
   oprawa_row,
-  handleChangeCardOprawa
-})
-
-{
-  function wydzielOprawe(){
-
+  handleChangeCardOprawa,
+}) {
+  function wydzielOprawe() {
     const newOprawa = oprawa.slice();
 
-    // newOprawa.map((x) => {
-    //   if (x.index > oprawa_row.index) {
-    //     return {
-    //       ...x, 
-
-    //     };
-    //   } else {
-    //     return x;
-    //   }
-    // });
-  
-    //     newOprawa
-    //     .map((t) => {
-    //   if (t.id == oprawa_row.id) {
-    //     // return {...t, uwagi: parseInt(t.naklad) - parseInt(wydziel)};
-    //      return {...t, uwagi: 100};
-    //   } else {
-    //     return t;
-    //   }
-    // })
-
-    newOprawa
-    .push({
+    newOprawa.push({
       id: Math.max(...newOprawa.map((f) => f.id)) + 1,
       zamowienie_id: oprawa_row.zamowienie_id,
       produkt_id: oprawa_row.produkt_id,
       oprawa: oprawa_row.oprawa,
       bok_oprawy: oprawa_row.bok_oprawy,
-  
+
       naklad: wydziel,
       index: Math.max(...newOprawa.map((f) => f.index)) + 1,
       uwagi: oprawa_row.uwagi,
       data_spedycji: oprawa_row.data_spedycji,
-    
     });
-  
+
     newOprawa.sort((a, b) => a.index - b.index);
-    
-
-
-    // setOprawa(newOprawa);
 
     setOprawa(
       newOprawa.map((t) => {
         if (t.id == oprawa_row.id) {
-          return {...t, naklad: parseInt(oprawa_row.naklad) - parseInt(wydziel)};
+          return {
+            ...t,
+            naklad: parseInt(oprawa_row.naklad) - parseInt(wydziel),
+          };
         } else {
           return t;
         }
       })
     );
 
+    let id_nowej_oprawy = Math.max(...fragmenty.map((f) => f.id)) + 1;
 
+    const newFragmenty = fragmenty.slice();
 
-    let id_nowej_oprawy = Math.max(...fragmenty.map((f) => f.id)) + 1
-    
+    newFragmenty.map((fragment) => {
+      if (fragment.oprawa_id == oprawa_row.id) {
+        newFragmenty.push({
+          id: Math.max(...newFragmenty.map((f) => f.id)) + 1,
+          zamowienie_id: fragment.zamowienie_id,
+          element_id: fragment.element_id,
+          oprawa_id: id_nowej_oprawy,
+          naklad: wydziel,
+          typ: fragment.typ,
+          index: parseInt(fragment.index) + 1,
+        });
+      }
+    });
 
-         const newFragmenty = fragmenty.slice();
-         
-         newFragmenty.map((fragment) =>{
-          if (fragment.oprawa_id == oprawa_row.id) {
-          newFragmenty.push({
-            id: Math.max(...newFragmenty.map((f) => f.id)) + 1,
-            zamowienie_id: fragment.zamowienie_id,
-            element_id: fragment.element_id,
-            oprawa_id: id_nowej_oprawy,
-            naklad: wydziel,
-            typ: fragment.typ,
-            index: parseInt(fragment.index) + 1
-  
-          })
-         }}
+    setFragmenty((prev) =>
+      prev.map((t) => {
+        if (t.oprawa_id == oprawa_row.id) {
+          return {
+            ...t,
+            naklad: parseInt(oprawa_row.naklad) - parseInt(wydziel),
+          };
+        } else {
+          return t;
+        }
+      })
+    );
 
+    //  setFragmenty(newFragmenty);
+    //    setFragmenty((prev)=> prev.map((fragment) =>{
+    //     if (fragment.oprawa_id == oprawa_row.id) {
+    //     newFragmenty.push({
+    //       id: Math.max(...newFragmenty.map((f) => f.id)) + 1,
+    //       zamowienie_id: fragment.zamowienie_id,
+    //       element_id: fragment.element_id,
+    //       oprawa_id: id_nowej_oprawy,
+    //       naklad: wydziel,
+    //       typ: fragment.typ,
+    //       index: parseInt(fragment.index) + 1
 
-         )
+    //         })
+    //    }
+    //  }) )
+    //  setFragmenty(newFragmenty);
 
+    //   }
+    // }) )
 
-         setFragmenty((prev) =>  prev.map((t) => {
-          if (t.oprawa_id == oprawa_row.id) {
-            return {...t, naklad: parseInt(oprawa_row.naklad) - parseInt(wydziel)};
-          } else {
-            return t;
-          }
-        })
-        )
-
-        //  setFragmenty(newFragmenty);
-      //    setFragmenty((prev)=> prev.map((fragment) =>{
-      //     if (fragment.oprawa_id == oprawa_row.id) {
-      //     newFragmenty.push({
-      //       id: Math.max(...newFragmenty.map((f) => f.id)) + 1,
-      //       zamowienie_id: fragment.zamowienie_id,
-      //       element_id: fragment.element_id,
-      //       oprawa_id: id_nowej_oprawy,
-      //       naklad: wydziel,
-      //       typ: fragment.typ,
-      //       index: parseInt(fragment.index) + 1
-  
-      //         })
-      //    } 
-      //  }) )
-        //  setFragmenty(newFragmenty);
-
-      //   } 
-      // }) )
-
-      // setFragmenty((prev) =>  prev.map((fragment) => {
-      //   if (fragment.oprawa_id == oprawa_row.id) {
-     
-      //     newFragmenty.push({
-      //       id: Math.max(...newFragmenty.map((f) => f.id)) + 1,
-      //       zamowienie_id: fragment.zamowienie_id,
-      //       element_id: fragment.element_id,
-      //       oprawa_id: id_nowej_oprawy,
-      //       naklad: wydziel,
-      //       typ: fragment.typ,
-      //       index: parseInt(fragment.index) + 1
-  
-      //     })
-      //   } 
-      // }) )
- 
-      // newFragmenty
-      // .map((t) => {
-      //   if (t.oprawa_id == oprawa_row.id) {
-      //     return {...t, naklad: parseInt(oprawa_row.naklad) - parseInt(wydziel)};
-      //   } else {
-      //     return t;
-      //   }
-      // })
-    //   fragmenty.map((fragment) => {
+    // setFragmenty((prev) =>  prev.map((fragment) => {
     //   if (fragment.oprawa_id == oprawa_row.id) {
-   
+
     //     newFragmenty.push({
     //       id: Math.max(...newFragmenty.map((f) => f.id)) + 1,
     //       zamowienie_id: fragment.zamowienie_id,
@@ -169,37 +113,45 @@ export default function OprawaElementyStage({
     //       index: parseInt(fragment.index) + 1
 
     //     })
-    //   } 
-    // }) 
+    //   }
+    // }) )
 
+    // newFragmenty
+    // .map((t) => {
+    //   if (t.oprawa_id == oprawa_row.id) {
+    //     return {...t, naklad: parseInt(oprawa_row.naklad) - parseInt(wydziel)};
+    //   } else {
+    //     return t;
+    //   }
+    // })
+    //   fragmenty.map((fragment) => {
+    //   if (fragment.oprawa_id == oprawa_row.id) {
 
+    //     newFragmenty.push({
+    //       id: Math.max(...newFragmenty.map((f) => f.id)) + 1,
+    //       zamowienie_id: fragment.zamowienie_id,
+    //       element_id: fragment.element_id,
+    //       oprawa_id: id_nowej_oprawy,
+    //       naklad: wydziel,
+    //       typ: fragment.typ,
+    //       index: parseInt(fragment.index) + 1
 
-
-
+    //     })
+    //   }
+    // })
   }
 
+  const [wydziel, setWdziel] = useState();
 
-
-
-
-
-
-  const [wydziel,setWdziel] =useState();
-  
   return (
-    
     <div className={style.insertContainer}>
       <div className={style.header}>
         {" "}
         <p className={style.title}>Podziel oprawę </p>
       </div>
-      <Wydziel wydziel={wydziel} setWdziel={setWdziel}/> 
+      <Wydziel wydziel={wydziel} setWdziel={setWdziel} />
 
-      <div className={style.center}>
-        
-      </div>
-
-
+      <div className={style.center}></div>
 
       <div className={style.row}>
         <button
@@ -223,21 +175,19 @@ export default function OprawaElementyStage({
     </div>
   );
 }
-function Wydziel({wydziel,setWdziel}){
-  
-  
-  return(
-      <div className={style.col}>
-      <label className={style.label}> Wydziel z nakładu:
-       </label>
-      <input placeholder="0 szt."className={style.data} type="text"
-      //  value="0"
-      onChange={(event) => {
-        setWdziel(event.target.value);
-      }}></input>
+function Wydziel({ wydziel, setWdziel }) {
+  return (
+    <div className={style.col}>
+      <label className={style.label}> Wydziel z nakładu:</label>
+      <input
+        placeholder="0 szt."
+        className={style.data}
+        type="text"
+        //  value="0"
+        onChange={(event) => {
+          setWdziel(event.target.value);
+        }}
+      ></input>
     </div>
   );
 }
-
-
-

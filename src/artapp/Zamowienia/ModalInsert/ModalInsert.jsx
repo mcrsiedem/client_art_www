1af83f8924ac55faf33715f81feade144acd3f5b
Maciek <siedem@onet.pl>
2024-02-11 17:50:05 +0100

    zapis

--- src/artapp/Zamowienia/ModalInsert/ModalInsert.jsx
+++ src/artapp/Zamowienia/ModalInsert/ModalInsert.jsx
@@ -540,39 +540,39 @@ const [openModalStany, setOpenModalStany] = useState(false);
                                     //--------------------------- element end
                               
                       //--------------------------- oprawa
-                              //  oprawaEdit
-                              // .forEach(async (opr, i) => {
-                              //   let oprawa_id_przed  =opr.id ;
-                              //   let res5 = await axios.post(ip + "oprawa", {
-                              //       zamowienie_id: zamowienie_id,
-                              //       produkt_id: produkt_id,
-                              //       oprawa: opr.oprawa,
-                              //       naklad: opr.naklad,
-                              //       uwagi: opr.uwagi,
-                              //       data_spedycji: opr.data_spedycji
-                              //     });
-                              //       let oprawa_id = res5.data.insertId;
+                               oprawaEdit
+                              .forEach(async (opr, i) => {
+                                let oprawa_id_przed  =opr.id ;
+                                let res5 = await axios.post(ip + "oprawa", {
+                                    zamowienie_id: zamowienie_id,
+                                    produkt_id: produkt_id,
+                                    oprawa: opr.oprawa,
+                                    naklad: opr.naklad,
+                                    uwagi: opr.uwagi,
+                                    data_spedycji: opr.data_spedycji
+                                  });
+                                    let oprawa_id = res5.data.insertId;
                                       
-                              //       let indexof = oprawa.indexOf(opr);
-                              //       oprawaEdit[indexof].id = oprawa_id
-                              //       oprawaEdit[indexof].id_prev = oprawa_id_przed
-                              //       oprawaEdit[indexof].zamowienie_id = zamowienie_id
-                              //       oprawaEdit[indexof].produkt_id = produkt_id
+                                    let indexof = oprawa.indexOf(opr);
+                                    oprawaEdit[indexof].id = oprawa_id
+                                    oprawaEdit[indexof].id_prev = oprawa_id_przed
+                                    oprawaEdit[indexof].zamowienie_id = zamowienie_id
+                                    oprawaEdit[indexof].produkt_id = produkt_id
 
                                     
          
-                              //       console.log("Koniec oprawy")
+                                    console.log("Koniec oprawy")
               
-                              //       console.log("Przystane 3")
-                              //          dane[0].test = 23;
-                              //          console.log("Przystane 4")
-                              //          console.log( dane)
-                              //          console.log("Przystane 5")
-                              //         //  console.log("postOprawatest: "+ postOprawatest(2))
+                                    console.log("Przystane 3")
+                                       dane[0].test = 23;
+                                       console.log("Przystane 4")
+                                       console.log( dane)
+                                       console.log("Przystane 5")
+                                      //  console.log("postOprawatest: "+ postOprawatest(2))
 
-                              // });
+                              });
 
-                       oprawaEdit =        postOprawa(oprawaEdit,ip,zamowienie_id,produkt_id,oprawa,dane)
+                      //  oprawaEdit =        postOprawa(oprawaEdit,ip,zamowienie_id,produkt_id,oprawa,dane)
 
                     setOprawa(oprawaEdit)
                        //--------------------------- oprawa end

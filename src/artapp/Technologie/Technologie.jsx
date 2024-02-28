d5b1facf575cb9403eeaf5a69301af500a6f4ca0
Maciek <maciek@artdruk.eu>
2024-02-28 14:05:37 +0100

    zapis

--- src/artapp/Technologie/Technologie.jsx
+++ src/artapp/Technologie/Technologie.jsx
@@ -2,26 +2,36 @@
 import React, { useState, useEffect, useRef } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import axios from "axios";
+import { ip } from "../../Host";
+import TechTable from "./components/TechTable/TechTable";
 
 export default function Technologie(){
 
+  const [dataTechnologie,setDataTechnologie] =useState([]);
     const effectRan = useRef(false);
     useEffect(() => {
       if (effectRan.current === true) {
-        // console.log("value: "+ value)
-     //   console.log("Test tokenu"+ token.token)
-        //console.log("Test pojedynczego rendera Header")
+         fetchTechnologie();
+
       }
       return () => {
         effectRan.current = true;
       };
     }, []);
 
+   async function fetchTechnologie(){
+      const res = await axios.get(ip + "technologie");
+      let job =[...res.data]
+      setDataTechnologie(job)
+      console.log(job)
+    }
 
 
     return(
     <>
         <Header/>
+        <TechTable dataTechnologie={dataTechnologie}/>
         <Footer/>
 
       
---
+++ src/artapp/Technologie/components/TechTable/TechTable.jsx
@@ -0,0 +1,44 @@
+// tableka z technologiami
+
+import style from "./TechTable.module.css";
+export default function TechTable({dataTechnologie}){
+return(
+    <table>
+    <thead>
+      <tr>
+        <th className={style.col_id}>#</th>
+        <th className={style.col_nr}>Nr</th>
+        <th className={style.col_rok}>Rok</th>
+        <th className={style.col_tytul}>tytul</th>
+       
+
+      </tr>
+    </thead>
+    <tbody>
+      {dataTechnologie.map((row) => {
+        return (
+          <tr 
+            key={row.id}
+            onDoubleClick={(node, event) => {
+              
+            //   open2(row.id);
+            //   setRow({ id: row.id});
+            }}
+            onClick={()=> {
+                // setRow(row.id)
+            console.log(row.id)
+            }}
+          >
+            <td>{row.id} </td>
+            <td>{row.nr} </td>
+            <td>{row.rok} </td>
+   
+            <td>{row.tytul}</td>
+
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
+)
+}

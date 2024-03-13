c47fc35c08b760e362ab2f211cd1eeba3360ca00
Maciek <maciek@artdruk.eu>
2024-03-13 08:27:56 +0100

    login

--- src/artapp/Login/Login.js
+++ src/artapp/Login/Login.js
@@ -20,33 +20,14 @@ export default function Login({user,setUser}) {
  let socket;
   const context = useContext(TokenContext);
 
-  function parseJwt(token) {
-    //wyciaga payload z tokenu
-    var base64Url = token.split(".")[1];
-    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    var jsonPayload = decodeURIComponent(
-      window
-        .atob(base64)
-        .split("")
-        .map(function (c) {
-          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join("")
-    );
-
-    return JSON.parse(jsonPayload);
-  }
-
-
   const pokazSocket = () =>{
     context.setSocketStan(socket)
     console.log(context.socketStan)
   }
   useEffect(() => {
-    // socket = io();
+  
      socket = io.connect(ip_socket_io)
      context.setSocketStan(socket)
- 
     header = document.getElementById("header");
     header.style.display = "none";
   }, []);
@@ -71,63 +52,33 @@ export default function Login({user,setUser}) {
     haslo: "",
   });
 
-  //   axios.defaults.withCredentials= true;
-
   const navigate = useNavigate();
-
+  
   const handleSubmit = (event,socekt) => {
     event.preventDefault();
     axios.get(ip + "users/" + values.login + "/" + values.haslo).then((res) => {
       if (res.data.length > 0) {
-       //   token.setToken(parseJwt(res.data).id);
-        // localStorage.setItem('header', true)
-        // setCookie("token", res.data, { path: "/" });
-
-        // sessionStorage.setItem("id", parseJwt(res.data).id); // tymczasowo zapisje id usera
         sessionStorage.setItem("id", DecodeToken(res.data).id); // tymczasowo zapisje id usera
         sessionStorage.setItem("token", res.data); // token w sesionStorage aby kazda karta przegladarki wymagala zalogowania
 
-        // przechowuje usera w contexcie ale po odswieżeniu context jest pusty
-        // token.setUser({
-        //   id:parseJwt(res.data).id,
-        //   imie:parseJwt(res.data).imie,
-        //   nazwisko:parseJwt(res.data).nazwisko,
-        //   login:parseJwt(res.data).login,
-        //   dostep:parseJwt(res.data).dostep,
-
-        //   })
-
         header.style.display = "grid";
         navigate("/Panel");
-        // pokazSocket()
-        // socket.connect(ip_socket_io)
+
 
       } else {
         console.log("Błąd");
       }
-      // console.log(res);
+
     });
   };
 
   return (
     <div className={style.container}>
-      {/* <div className={style.header}>
-        <Header/>
-      </div> */}
       <div className={style.center}>
-      
       <Center values={values} setValues={setValues} handleSubmit={handleSubmit}/>
       </div>
-
     </div>
   );
-}
-
-function Header(){
- return (<>
-
-  
-  </>)
 }
 
 

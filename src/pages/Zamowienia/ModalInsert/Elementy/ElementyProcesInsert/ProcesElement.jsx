5577dfcfb3800bc497ce15d0bf9d499eeba63fb7
Maciek <siedem@onet.pl>
2024-03-16 20:36:23 +0100

    proces element

--- src/pages/Zamowienia/ModalInsert/Elementy/ElementyProcesInsert/ProcesElement.jsx
+++ src/pages/Zamowienia/ModalInsert/Elementy/ElementyProcesInsert/ProcesElement.jsx
@@ -1,120 +1,47 @@
-import React, { useEffect, useState, useContext,useRef } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import style from "./ProcesElement.module.css";
-
-import iconX from 'assets/xDark.svg'
+import iconX from "assets/xDark.svg";
 import { AppContext } from "context/AppContext";
 import { ModalInsertContext } from "context/ModalInsertContext";
 export default function ProcesElement() {
 
-  const contexApp = useContext(AppContext);
-  const procesyElementow = contexApp.process;
-  
-  const modalContext = useContext(ModalInsertContext);
-  const listaDostepnychProcesow = contexApp.process;
- 
 
   return (
-    <div className={style.window}>
-      <Header/>
-      <div className={style.main}>
-        <table className={style.table}>
-          <thead>
-            <tr>
-              <th className={style.col1}>#</th>
-              <th className={style.col_typ}>Proces</th>
-              <th className={style.col_typ}>Typ</th>
-              <th className={style.col_naklad}>Front</th>
-              <th className={style.col_naklad}>Nazwa</th>
-              <th className={style.col_naklad}>Back</th>
-              <th className={style.col_naklad}>Nazwa</th>
-              <th className={style.col_wersja}>Uwagi</th>
-
-            </tr>
-          </thead>
-          <tbody>
-            {
-                procesyElementow.map((row, i) => {
-                  return (
-                  <tr key={row.id}>
-                <td>{row.id}</td>
-                    <td>{row.proces_nazwa}</td>
-                    <td>{row.proces_typ}</td>
-                    <td>4</td>
-                    <td>CMYK</td>
-                    <td>CMYK</td>
-                    <td>CMYK</td>
-                    <td></td>
-                  </tr>)
-                })
-            }
-            {/* <tr>
-              <td>1</td>
-              <td>Druk</td>
-              <td>farba</td>
-              <td>CMYK</td>
-              <td>CMYK</td>
-              <td></td>
-
-            </tr>
-
-            <tr>
-              <td>3</td>
-              <td>Uszlachetnianie</td>
-              <td>lakier</td>
-              <td>UV</td>
-              <td></td>
-              <td></td>
-
-            </tr> */}
-            {/* {elementy.map((row, i) => {
-            return (
-              <RowElement
-                i={i}
-                row={row}
-                handleChangeCardElementy={handleChangeCardElementy}
-                listaPapierow={listaPapierow}
-                listaGramatur={listaGramatur}
-                setListaGramatur={setListaGramatur}
-                isEdit={isEdit}
-                setIsEdit={setIsEdit}
-                procesyElementow={procesyElementow}
-                setProcesyElementow={setProcesyElementow}
-                listaDostepnychProcesow={listaDostepnychProcesow}
-                setShćalementyProcesyInsert={setShowElementyProcesyInsert}
-              />
-            );
-          })} */}
-          </tbody>
-        </table>
-      </div>
-      <div className={style.footer}>
-<Zapisz/>
-      
-      </div>
-    </div>
+   <Window>
+      <Header />
+      <Table />
+      <Zapisz />
+   </Window>
   );
 }
 
 
-function Header({ setShowElementyProcesyInsert }) {
+function Window({children}) {
+
+  return (
+    <div className={style.window}>
+    {children}
+    </div>
+  );
+}
+function Header() {
   const modalContext = useContext(ModalInsertContext);
   const idElementu = modalContext.selectedElementID;
   return (
     <div className={style.header}>
-      <p className={style.title}>Procesy  {idElementu}</p>
-      <ZamknijX setShowElementyProcesyInsert={setShowElementyProcesyInsert}/>
+      <p className={style.title}>Procesy {idElementu}</p>
+      <Zamknij/>
     </div>
   );
 }
-function ZamknijX() {
+function Zamknij() {
   const modalContext = useContext(ModalInsertContext);
   return (
     <img
       className={style.zamknij_icon}
       src={iconX}
       onClick={() => {
-        modalContext.setShowElementyProcesyInsert(false)
-        // setShowElementyProcesyInsert(false);
+        modalContext.setShowElementyProcesyInsert(false);
       }}
       alt="Procesy"
     />
@@ -124,15 +51,55 @@ function ZamknijX() {
 function Zapisz() {
   const modalContext = useContext(ModalInsertContext);
   return (
-    
-    <button
-      className={style.btn}
-      onClick={() => {
-        modalContext.setShowElementyProcesyInsert(false);
-      }}
-    >
-      Zapisz
-    </button>
+    <div className={style.footer}>
+      <button
+        className={style.btn}
+        onClick={() => {
+          modalContext.setShowElementyProcesyInsert(false);
+        }}
+      >
+        Zapisz
+      </button>
+    </div>
+  );
+}
+
+function Table() {
+  const contexApp = useContext(AppContext);
+  const procesyElementow = contexApp.process;
+  return (
+    <div className={style.main}>
+      <table className={style.table}>
+        <thead>
+          <tr>
+            <th className={style.col1}>#</th>
+            <th className={style.col_typ}>Proces</th>
+            <th className={style.col_typ}>Typ</th>
+            <th className={style.col_naklad}>Front</th>
+            <th className={style.col_naklad}>Nazwa</th>
+            <th className={style.col_naklad}>Back</th>
+            <th className={style.col_naklad}>Nazwa</th>
+            <th className={style.col_wersja}>Uwagi</th>
+          </tr>
+        </thead>
+        <tbody>
+          {procesyElementow.map((row, i) => {
+            return (
+              <tr key={row.id}>
+                <td>{row.id}</td>
+                <td>{row.proces_nazwa}</td>
+                <td>{row.proces_typ}</td>
+                <td>4</td>
+                <td>CMYK</td>
+                <td>CMYK</td>
+                <td>CMYK</td>
+                <td></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 // function ProcesSelect({

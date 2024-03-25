6f0d11453397b6903d51885415bd16a6d6c13747
Maciek <siedem@onet.pl>
2024-03-25 22:07:42 +0100

    components

--- src/components/ProductCreator/ProductSelector/ProductSelector.jsx
+++ src/components/ProductCreator/ProductSelector/ProductSelector.jsx
@@ -1,6 +1,6 @@
 
 import style from './ProductSelector.module.css'
-import ProductTypeCard from './components/ProductTypeCard'
+import ProductTypeCard from '../components/ProductTypeCard'
 export default function ProductSelector(){
     return(
         <div className={style.container}>

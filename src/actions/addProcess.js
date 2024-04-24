0d2e831158b8ce66483ae1c71ae8c93d85c2b447
Maciek <maciek@artdruk.eu>
2024-04-24 11:35:41 +0200

    add process

--- src/actions/addProcess.js
+++ src/actions/addProcess.js
@@ -3,41 +3,52 @@ import axios from "axios";
 import { IP } from "../utils/Host";
 
 
-export function addNewProcess(row, pakowanie, setPakowanie,procesyElementow, setProcesyElementow) {
-const pakowanieEdit = pakowanie.slice();
+export function addNewProcess(row,procesyElementow, setProcesyElementow) {
+
 const procesyElementowEdit = procesyElementow.slice();
 
   axios
-    .post(IP + "pakowanie", {
+    .post(IP + "proces", {
+      id:0,
       zamowienie_id: 0,
       produkt_id: 0,
-      nazwa: 0,
-      naklad: 0,
-      sztuki_w_paczce: 0,
-      rodzaj_pakowania: 0,
-      uwagi: "pakowanie temp",
+      element_id: 1,
+      proces_id: 1,
+      nazwa_id: 1,
+      proces_nazwa: 2,
+      proces_typ: 1,
+      front_ilosc: "",
+      back_ilosc: "4",
+      front_kolor: "CMYK",
+      back_kolor: "CMYK",
+      info: "",
       indeks: 0,
     })
     .then((res) => {
-      pakowanieEdit.map((p) => {
+      procesyElementowEdit.map((p) => {
         if (p.indeks > row.indeks) {
           p.indeks++;
         }
       });
 
-      pakowanieEdit.push({
-        id: Math.max(...pakowanie.map((f) => f.id)) + 1,
+      procesyElementowEdit.push({
+        id: Math.max(...procesyElementow.map((f) => f.id)) + 1,
         zamowienie_id: row.zamowienie_id,
         produkt_id: row.produkt_id,
-        nazwa: row.nazwa,
-        naklad: row.naklad,
-        sztuki_w_paczce: row.sztuki_w_paczce,
-        rodzaj_pakowania: row.rodzaj_pakowania,
-        uwagi: row.uwagi,
+        element_id: row.element_id,
+        proces_id: row.proces_id,
+        nazwa_id: row.nazwa_id,
+        proces_nazwa: row.proces_nazwa,
+        proces_typ: row.proces_typ,
+        front_ilosc: row.front_ilosc,
+        back_ilosc: row.back_ilosc,
+        front_kolor: row.front_kolor,
+        back_kolor: row.back_kolor,
+        info: row.info,
         indeks: row.indeks + 1,
       });
 
-      pakowanieEdit.sort((a, b) => a.indeks - b.indeks);
-      setPakowanie(pakowanieEdit);
+      procesyElementowEdit.sort((a, b) => a.indeks - b.indeks);
+      setProcesyElementow(procesyElementowEdit);
     });
 }

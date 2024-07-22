529297eabd9a32f228dbb57fba1b2ba59d0797c0
Maciek <maciek@artdruk.eu>
2024-07-22 10:11:42 +0200

    usuwanie procesu

--- src/actions/deleteProcessTech.js
+++ src/actions/deleteProcessTech.js
@@ -3,8 +3,10 @@ export function deleteProcessTech(
   procesyElementowTechTemporary,
   setProcesyElementowTechTemporary
 ) {
-  const procesyElementowEditTemporary = procesyElementowTechTemporary.slice();
-  procesyElementowEditTemporary.filter((p) => p.id == row.id);
-  procesyElementowEditTemporary.sort((a, b) => a.indeks - b.indeks);
-  setProcesyElementowTechTemporary(procesyElementowEditTemporary);
+  // const procesyElementowEditTemporary = procesyElementowTechTemporary.slice();
+  // procesyElementowEditTemporary.filter((p) => p.id == row.id);
+  // procesyElementowEditTemporary.sort((a, b) => a.indeks - b.indeks);
+console.log("procesy ", procesyElementowTechTemporary)
+console.log("proces_id "+ row.id)
+   setProcesyElementowTechTemporary(procesyElementowTechTemporary.filter((p) => p.id !== row.id));
 }

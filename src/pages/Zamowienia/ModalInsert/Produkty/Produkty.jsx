b342263f606bb6ceb6a7a1ea73e0df28607d8064
Maciek <maciek@artdruk.eu>
2024-08-26 11:12:25 +0200

    oprawa

--- src/pages/Zamowienia/ModalInsert/Produkty/Produkty.jsx
+++ src/pages/Zamowienia/ModalInsert/Produkty/Produkty.jsx
@@ -10,10 +10,11 @@ export default function Produkty( ) {
       <div className={style.container}>
             <div className={style.produkt}>
               {/* <ProduktyTableHeader /> */}
-              <hr></hr>
+    
               <p>Produkt</p>
          
               <ProduktyTable2   />
+           
             </div>
     </div>
   );
@@ -38,7 +39,7 @@ const produkty = contextModalInsert.produkty;
 
                   <Typ2 row={produkty[0]} />
                   <Naklad2 row={produkty[0]} />
-                  <Oprawa2 row={produkty[0]} />
+                  <RodzajOprawy row={produkty[0]} />
                   <Nazwa2 row={produkty[0]} />
                   <td>{produkty[0].ilosc_stron}</td>
                   <td>{produkty[0].format_x}</td>
@@ -52,72 +53,9 @@ const produkty = contextModalInsert.produkty;
 
 }
 
-function ProduktyTable() {
-  const contextModalInsert = useContext(ModalInsertContext);
-const produkty = contextModalInsert.produkty;
-  return <div className={style.main}>
-      
-        <table className={style.table}>
-          <thead>
-            <tr>
-              <th className={style.col3}>Typ</th>
-              <th className={style.col10}>Nakład</th>
-              <th className={style.col9}>Oprawa</th>
-              <th className={style.col4}>Nazwa</th>
-              <th className={style.col6}>Strony</th>
-              <th className={style.col7}>Szerokość</th>
-              <th className={style.col8}>Wysokość</th>
-              <th className={style.col8}>Uwagi</th>
-            </tr>
-          </thead>
-          <tbody className={style.center}>
-            {produkty.map((row) => {
-              return (
-                <tr key={row.id}>
-                  <Typ row={row} />
-                  <Naklad row={row} />
-                  <Oprawa row={row} />
-                  <Nazwa row={row} />
-                  <td>{row.ilosc_stron}</td>
-                  <td>{row.format_x}</td>
-                  <td>{row.format_y}</td>
-                  <Uwagi row={row} />
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
 
-}
 
-function Typ({ row }) {
-  const contextModalInsert = useContext(ModalInsertContext);
-  const contextApp = useContext(AppContext);
 
-  const handleUpdateRowProdukty = contextModalInsert.handleUpdateRowProdukty;
-  return (
-    <td>
-      <select
-        className={style.select}
-        defaultValue={row.typ}
-        onChange={(e) => {
-          handleUpdateRowProdukty({
-            ...row,
-            typ: e.target.value,
-          });
-        }}
-      >
-        {}
-        {contextApp.productType.map((option) => (
-          <option key={option.id} value={option.id}>
-            {option.nazwa}
-          </option>
-        ))}
-      </select>
-    </td>
-  );
-}
 
 function Typ2({ row }) {
   const contextModalInsert = useContext(ModalInsertContext);
@@ -125,7 +63,8 @@ function Typ2({ row }) {
 
   const handleUpdateRowProdukty = contextModalInsert.handleUpdateRowProdukty;
   return (
-    <div>
+    <div className={style.col_dane}>
+       <label className={style.label}> Typ </label>
       <select
         className={style.select}
         defaultValue={row.typ}
@@ -146,31 +85,7 @@ function Typ2({ row }) {
     </div>
   );
 }
-function Nazwa({ row }) {
-  const contextModalInsert = useContext(ModalInsertContext);
-  const handleUpdateRowProdukty = contextModalInsert.handleUpdateRowProdukty;
-  return (
-    <td>
-      <input
-        className={style.in}
-        value={row.nazwa}
-        onChange={(e) =>
-          {
-            
-            if ( e.target.value === '' || reg_txt.test(e.target.value)) {
-            handleUpdateRowProdukty({
-            ...row,
-            nazwa: e.target.value,
-          })
 
-          }
-        }
-    
-        }
-      ></input>
-    </td>
-  );
-}
 function Nazwa2({ row }) {
   const contextModalInsert = useContext(ModalInsertContext);
   const handleUpdateRowProdukty = contextModalInsert.handleUpdateRowProdukty;
@@ -196,39 +111,14 @@ function Nazwa2({ row }) {
     </div>
   );
 }
-function Naklad({ row }) {
-const contextModalInsert = useContext(ModalInsertContext);
-const handleUpdateRowProdukty = contextModalInsert.handleUpdateRowProdukty;
 
-  return (
-    <td>
-      <input
-        className={style.in}
-        value={row.naklad}
-        onChange={(e) =>{     
-
-          // const re = /^[0-9]+$/;
-          if (e.target.value === '' || reg_int.test(e.target.value)) {
-           handleUpdateRowProdukty({
-                ...row,
-                naklad: e.target.value,
-              })
-          }
-            
-            }
-        
-
-        }
-      ></input>
-    </td>
-  );
-}
 function Naklad2({ row }) {
   const contextModalInsert = useContext(ModalInsertContext);
   const handleUpdateRowProdukty = contextModalInsert.handleUpdateRowProdukty;
   
     return (
-      <div>
+      <div className={style.col_dane}>
+        <label className={style.label}> Nakład </label>
         <input
           className={style.in}
           value={row.naklad}
@@ -250,33 +140,49 @@ function Naklad2({ row }) {
       </div>
     );
   }
-function Oprawa({ row }) {
-  const contextApp = useContext(AppContext);
+
+
+
+
+
+function RodzajOprawy({ row,handleChangeCardOprawa}) {
+  const contextModalInsert = useContext(ModalInsertContext);
+const produkty = contextModalInsert.produkty;
+const setProdukty = contextModalInsert.setProdukty;
+const contextApp = useContext(AppContext);
 
   return (
-    <td>
-{ contextApp.bindingType.map((t) => {
-        if (t.id == row.oprawa) {
-          return t.nazwa;
-        
-        }
-      })}
+    <div className={style.col_dane}>
+<label className={style.label}> Typ </label>
+      <select
+        disabled
+        className={style.firma}
+        defaultValue={row.oprawa}
+        onChange={(event) => {
+          handleChangeCardOprawa({...row, oprawa: event.target.value});
 
-    </td>
-  );
-}
-function Oprawa2({ row }) {
-  const contextApp = useContext(AppContext);
 
-  return (
-    <div>
-{ contextApp.bindingType.map((t) => {
-        if (t.id == row.oprawa) {
-          return t.nazwa;
-        
-        }
-      })}
+          if(row.indeks == 0){
+          setProdukty(
+            produkty.map((p) => {
+              if (p.id === row.produkt_id) {
+                return {...p, oprawa:event.target.value};
+              } else {
+                return p;
+              }
+            })
+          );
+           
+          }
 
+        }}
+      >
+        {contextApp.bindingType.map((option) => (
+          <option key={option.id} value={option.id}>
+          {option.nazwa} 
+          </option>
+        ))}
+      </select>
     </div>
   );
 }

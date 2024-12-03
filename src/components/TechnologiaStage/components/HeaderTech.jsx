6b505a0e548b68488091356dbe8a6d815fd56706
Maciek <maciek@artdruk.eu>
2024-12-03 15:10:01 +0100

    karta

--- src/components/TechnologiaStage/components/HeaderTech.jsx
+++ src/components/TechnologiaStage/components/HeaderTech.jsx
@@ -21,7 +21,7 @@ export default function Header({}){
       <header className={style.headerMain}>
         <LeftPane>
          {/* <p>Karta technologiczna... {techContext.rowTechnologia?.id} {techContext.rowZamowienia?.id}</p> */}
-         <p>Karta technologiczna...  {techContext.dane?.id}</p> 
+         <p>Karta technologiczna  {techContext.dane?.id}</p> 
          
          {/* <button onClick={()=> console.log(techContext.dane.id)}> OK</button> */}
         </LeftPane>

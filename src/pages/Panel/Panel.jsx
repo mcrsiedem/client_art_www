081c6a135486ca05d2c8630f40b75484b6f4ae5f
Maciek <siedem@onet.pl>
2024-12-07 16:37:32 +0100

    panel

--- src/pages/Panel/Panel.jsx
+++ src/pages/Panel/Panel.jsx
@@ -4,6 +4,13 @@ import style from '../Panel/Panel.module.css';
 import logoutIcon from 'assets/logout.png'
 import userOnline from 'assets/user_offline.svg'
 import userOffline from 'assets/user_offline.svg'
+import iconZamowienia from 'assets/iconZamowienia.svg'
+import iconTechnolgie from 'assets/iconTechnologie.svg'
+import iconHistoria from 'assets/iconHistoria.svg'
+import iconUstawienia from 'assets/iconUstawienia.svg'
+import iconMagazyn from 'assets/iconMagazyn.svg'
+import iconCTP from 'assets/iconCTP.svg'
+import iconProcesy from 'assets/iconProcesy.svg'
 
 import DecodeToken from "pages/Login/DecodeToken";
 import { useNavigate } from "react-router-dom";
@@ -22,7 +29,6 @@ function Panel({user,setUser}){
       }
 
     return(<>
-
         <div className={style.main}>
                 <div className={style.header}>
 
@@ -38,18 +44,20 @@ function Panel({user,setUser}){
                
                 </div>
                         <div className={style.kafleContainer} >
+                            
+                            
+                                        <div className={style.kafle} onClick={() => { navigate("/Zamowienia") }}>
+                                        <img className={style.icon } src={iconZamowienia} alt="Zamówienia" />
+                                              <p>ZAMÓWIENIA</p>  
+                                                </div>
 
-                                        <div className={style.kafle} onClick={() => { navigate("/Zamowienia") }}>ZAMÓWIENIA</div>
-                                        <div className={style.kafle}  onClick={() => { navigate("/Technologie") } }>TECHNOLOGIE</div>
-                                        <div className={style.kafle} onClick={() => { navigate("/ProcesyView") }} >PROCESY </div> 
-                                        <div className={style.kafle} >CTP</div>
-                                        {/* <div className={style.kafle} >Falcowanie</div>
-                                        <div className={style.kafle} >Oprawa</div>
-                                        <div className={style.kafle} >Uszlachetnianie</div> */}
-                                        <div className={style.kafle} > MAGAZYN</div>
-                                        {/* <div className={style.kafle} onClick={() => { navigate("/info") }}>Info</div> */}
-                                        <div className={style.kafle} >USTAWIENIA</div>
-                                        <div className={style.kafle} >HISTORIA</div>
+             
+                                        <div className={style.kafle}  onClick={() => { navigate("/Technologie") } }><img className={style.icon } src={iconTechnolgie} alt="Technologie" /><p>TECHNOLOGIE</p></div>
+                                        <div className={style.kafle} onClick={() => { navigate("/ProcesyView") }} ><img className={style.icon } src={iconProcesy} alt="Zamówienia" /><p>PROCESY</p> </div> 
+                                        <div className={style.kafle} ><img className={style.icon } src={iconCTP} alt="CTP" /> <p>CTP</p></div>
+                                        <div className={style.kafle} ><img className={style.icon } src={iconMagazyn} alt="Magazyn" /><p>MAGAZYN</p> </div>
+                                        <div className={style.kafle} ><img className={style.icon } src={iconUstawienia} alt="Ustawienia" /><p>USTAWIENIA</p></div>
+                                        <div className={style.kafle} ><img className={style.icon } src={iconHistoria} alt="Zamówienia" /><p>HISTORIA</p></div>
                                         {/* <div className={style.kafle} ></div> */}
                                         
               

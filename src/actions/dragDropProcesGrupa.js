23ec00eade1aaab21f289e0bf7bcb234d08635de
Maciek <siedem@onet.pl>
2024-12-14 18:55:34 +0100

    drag

--- src/actions/dragDropProcesGrupa.js
+++ src/actions/dragDropProcesGrupa.js
@@ -10,12 +10,8 @@ export function dragDropProcesGrupa(id_drag_grupa_proces,id_drop_grupa_proces,fe
   axios
   .get(IP + "drag_drop_proces_grupa/"+id_drag_grupa_proces+"/"+id_drop_grupa_proces)
     .then((res) => {
-   
- 
-    console.log("procesor " +res.data[0].mcr)
+    // console.log("procesor " +res.data[0].mcr)
     fechGrupyAndWykonaniaForProcesor(res.data[0].mcr)
-
-
 
     });
 }

26c64b8ae7427ae6b628d18d8b4568e7832b60a6
Maciek <siedem@onet.pl>
2025-01-05 13:14:49 +0100

    klient elect

--- src/components/ProductCreator/ProductSelector/ProductSelector.jsx
+++ src/components/ProductCreator/ProductSelector/ProductSelector.jsx
@@ -8,14 +8,25 @@ import { initialPreOrder } from 'utils/initialvalue'
 export default function ProductSelector({show,setShow}){
     const poc = useContext(PreOrderContext)
     const mic = useContext(ModalInsertContext)
-    return(
+    const contextModalInsert = useContext(ModalInsertContext);
+const daneZamowienia = contextModalInsert.daneZamowienia;
+
+if (daneZamowienia.klient_id != 0) {
+            return(
+        
         <div onClick={()=>resetHandler(poc,mic)} className={style.container}>
+
             <ProductTypeCard typ={"gazeta"} show={show} setShow={setShow} />
             <ProductTypeCard typ={"ulotka"} show={show} setShow={setShow} />
             <ProductTypeCard typ={"pudelko"} show={show} setShow={setShow}/>
         </div>
-    )
+    ) 
 }
+         
+    
+}
+
+
 
 const resetHandler = (poc, mic) => {
     poc.setPreOrder(initialPreOrder)

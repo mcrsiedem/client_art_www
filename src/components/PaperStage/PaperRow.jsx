d38cc45ff467fde87c901f7ec20b8b3eeb7a8df6
Maciek <siedem@onet.pl>
2025-03-19 22:26:02 +0100

    zapis

--- src/components/PaperStage/PaperRow.jsx
+++ src/components/PaperStage/PaperRow.jsx
@@ -1,22 +1,142 @@
 import React, { useState, useContext } from "react";
 import { ModalInsertContext } from "context/ModalInsertContext";
 import style from "./ChangePaper.module.css";
+import style2 from "./TablePaperNazwa2.module.css";
+import { AppContext } from "context/AppContext";
 
 
 
-export default function ChangePaper({showChange,setShowChange,selectRow}) {
+export default function PaperRow({row,inputElement,setSelectTable,setSelectRow}) {
       const modalcontext = useContext(ModalInsertContext);
+        const appcontext = useContext(AppContext);
       const selectedElementROW = modalcontext.selectedElementROW;
+      const setListaPapierowWyszukiwarka = appcontext.setListaPapierowWyszukiwarka;
 
 
-    return (
-    <div className={style.window}>
-      <Header setShowChange={setShowChange}></Header>
-      <p className={style.alert_label}> {selectRow.nazwa} {selectRow.gramatura} g/m2 {selectRow.wykonczenie}</p>
-      <Zmien setShowChange={setShowChange} selectRow={selectRow}/>
+      return (
 
-    </div>
-  );
+        <div className={style2.row_papier}>
+          <tr
+            className={color2(row)}
+            key={row.id}
+            onClick={() => {
+              setSelectTable(inputElement);
+              setSelectRow(row);
+              setListaPapierowWyszukiwarka((prev) =>
+                prev
+                  .map((t, a) => {
+                    return {
+                      ...t,
+                      select: false,
+                    };
+                  })
+                  .map((t, a) => {
+                    if (t.id == row.id) {
+                      return {
+                        ...t,
+                        select: true,
+                      };
+                    } else {
+                      return t;
+                    }
+                  })
+              );
+            }}
+            // onDoubleClick={() =>
+            //   openEdit(row, rowID, setShowEdit)
+            // }
+          >
+            <ID row={row} />
+            {/* <Nazwa row={row} /> */}
+            <Gramatura row={row} />
+            <Wykonczenie row={row} />
+            <Bulk row={row} />
+            {/* <Grupa row={row} />  */}
+         
+            <Info row={row}  />
+          </tr>
+        </div>
+      );
 
 
 }
+function Gramatura({ row, index }) {
+  return <td>{row.gramatura}</td>;
+}
+function ID({ row,index}) {
+  return <td>{index}</td>;
+}
+function EXPAND({ row,index}) {
+  return <td>{row.isExpand}s</td>;
+}
+
+function Wykonczenie({ row }) {
+  return <td>{row.wykonczenie}</td>;
+}
+function Bulk({ row }) {
+  return <td>{row.bulk}</td>;
+}
+
+function Info({ row}) {
+  const appcontext = useContext(AppContext);
+  const setListaPapierowWyszukiwarka = appcontext.setListaPapierowWyszukiwarka;
+  const listaPapierowWyszukiwarka = appcontext.listaPapierowWyszukiwarka;
+    const modalcontext = useContext(ModalInsertContext);
+    // const isBtnZapiszPapierAvtive = modalcontext.isBtnZapiszPapierAvtive;
+    const setBtnZapiszPapierDisabled = appcontext.setBtnZapiszPapierDisabled;
+return (
+  <td className={style.labelinput}>
+
+    <input
+      className={style.select_papier_info}
+      type="text"
+      value={row.info}
+      onChange={(event) => {
+        const re = /^[a-zA-Z0-9_+\sąćęłńóśźżĄĘŁŃÓŚŹŻ"-.]+$/;
+        if (event.target.value === "" || re.test(event.target.value)) {
+          // setListaPapierowWyszukiwarka({ ...daneKlienta, firma: event.target.value });
+         
+          setListaPapierowWyszukiwarka(
+            listaPapierowWyszukiwarka.map((t, a) => {
+            // console.log("oprawa id" +prev)
+            if (t.id == row.id) {
+              return {
+                ...t,
+                info: event.target.value,
+                update: true
+      
+              };
+            } else {
+              return t;
+            }
+          })
+        );
+        setBtnZapiszPapierDisabled(false)
+
+        }
+      }}
+    >
+
+    </input>
+  </td>
+);
+}
+
+const color2 = (row) => {
+
+  if (row.select) {
+    return style2.tr_select;
+  }
+  if (row.delete) {
+    return style2.tr_delete;
+  }
+  if (row.insert) {
+    return style2.tr_insert;
+  }
+  if (row.update) {
+    return style2.tr_update;
+  }
+
+
+  return style2.tr;
+};
--- src/components/PaperStage/TablePaperNazwa.jsx
+++ src/components/PaperStage/TablePaperNazwa.jsx
@@ -10,6 +10,7 @@ import { AppContext } from "context/AppContext";
 import { ModalInsertContext } from "context/ModalInsertContext";
 import ChangePaper from "./ChangePaper";
 import { getMaxID } from "actions/getMaxID";
+import PaperRow from "./PaperRow";
 
 
 export default function TablePaperNazwa({
@@ -130,51 +131,11 @@ export default function TablePaperNazwa({
                   ?.filter((x) => x.nazwa_id == row2.id)
                   .map((row, index) => {
                     if (row2.isExpand) {
-
-                      
                       return (
+                        <PaperRow row={row} inputElement={inputElement} setSelectTable={setSelectTable} setSelectRow={setSelectRow}/>
+                      )
+                     
 
-                        <div className={style2.row_papier}>
-                          <tr
-                            className={color2(row)}
-                            key={row.id}
-                            onClick={() => {
-                              setSelectTable(inputElement);
-                              setSelectRow(row);
-                              setListaPapierowWyszukiwarka((prev) =>
-                                prev
-                                  .map((t, a) => {
-                                    return {
-                                      ...t,
-                                      select: false,
-                                    };
-                                  })
-                                  .map((t, a) => {
-                                    if (t.id == row.id) {
-                                      return {
-                                        ...t,
-                                        select: true,
-                                      };
-                                    } else {
-                                      return t;
-                                    }
-                                  })
-                              );
-                            }}
-                            onDoubleClick={() =>
-                              openEdit(row, rowID, setShowEdit)
-                            }
-                          >
-                            <ID row={row} index={index + 1} />
-                            <Nazwa row={row} />
-                            <Gramatura row={row} />
-                            <Wykonczenie row={row} />
-                            <Bulk row={row} />
-                            <Grupa row={row} />
-                            <Info row={row} setBtnZapisz={setBtnZapisz} />
-                          </tr>
-                        </div>
-                      );
                     }
                   })}
               </>
@@ -421,29 +382,11 @@ function Nazwa2({ row }) {
     </td>;
 }
 
-function Gramatura({ row, index }) {
-  return <td>{row.gramatura}</td>;
-}
 
 function ID({ row,index}) {
   return <td>{index}</td>;
 }
-function EXPAND({ row,index}) {
-  return <td>{row.isExpand}s</td>;
-}
 
-function Wykonczenie({ row }) {
-  return <td>{row.wykonczenie}</td>;
-}
-function Bulk({ row }) {
-  return <td>{row.bulk}</td>;
-}
-function Info2({ row }) {
-  return <td>{row.info}</td>;
-}
-function Opiekun({ row }) {
-  return <td>{row.opiekun_nazwa}</td>;
-}
 
 
 function Nazwa({ row,setBtnZapisz}) {
@@ -493,50 +436,7 @@ function Nazwa({ row,setBtnZapisz}) {
 
 
 //-------------------------------- tu
-function Info({ row}) {
-  const appcontext = useContext(AppContext);
-  const setListaPapierowWyszukiwarka = appcontext.setListaPapierowWyszukiwarka;
-  const listaPapierowWyszukiwarka = appcontext.listaPapierowWyszukiwarka;
-    const modalcontext = useContext(ModalInsertContext);
-    // const isBtnZapiszPapierAvtive = modalcontext.isBtnZapiszPapierAvtive;
-    const setBtnZapiszPapierDisabled = appcontext.setBtnZapiszPapierDisabled;
-return (
-  <td className={style.labelinput}>
 
-    <input
-      className={style.select_papier_info}
-      type="text"
-      value={row.info}
-      onChange={(event) => {
-        const re = /^[a-zA-Z0-9_+\sąćęłńóśźżĄĘŁŃÓŚŹŻ"-.]+$/;
-        if (event.target.value === "" || re.test(event.target.value)) {
-          // setListaPapierowWyszukiwarka({ ...daneKlienta, firma: event.target.value });
-         
-          setListaPapierowWyszukiwarka(
-            listaPapierowWyszukiwarka.map((t, a) => {
-            // console.log("oprawa id" +prev)
-            if (t.id == row.id) {
-              return {
-                ...t,
-                info: event.target.value,
-                update: true
-      
-              };
-            } else {
-              return t;
-            }
-          })
-        );
-        setBtnZapiszPapierDisabled(false)
-
-        }
-      }}
-    >
-
-    </input>
-  </td>
-);
-}
 
 const color = (row) => {
 
@@ -557,21 +457,3 @@ const color = (row) => {
   return style.tr;
 };
 
-const color2 = (row) => {
-
-  if (row.select) {
-    return style2.tr_select;
-  }
-  if (row.delete) {
-    return style2.tr_delete;
-  }
-  if (row.insert) {
-    return style2.tr_insert;
-  }
-  if (row.update) {
-    return style2.tr_update;
-  }
-
-
-  return style2.tr;
-};

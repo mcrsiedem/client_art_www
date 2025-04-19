5b5bbbc035b723d2046e87576f02fac1a42c0d6d
Maciek <siedem@onet.pl>
2025-04-19 22:55:00 +0200

    zapis

--- src/hooks/useIntroligatornia.js
+++ src/hooks/useIntroligatornia.js
@@ -3,19 +3,36 @@ import axios from "axios";
 import { IP } from "../utils/Host";
 import { AppContext } from "context/AppContext";
 import { useZamowienia } from "./useZamowienia";
+import { TechnologyContext } from "context/TechnologyContext";
 export function useIntroligatornia() {
-  const contextApp = useContext(AppContext);
-  const setUzytkownicy = contextApp.setUzytkownicy;
-  const setUzytkownicyGrupy = contextApp.setUzytkownicyGrupy;
+  const techContext = useContext(TechnologyContext);
 
-  const [refreshZamowienia] = useZamowienia();
-
+  const elementyTech = techContext.elementyTech;
+  const fragmentyTech = techContext.fragmentyTech;
+  const legiFragmenty = techContext.legiFragmenty;
+  const setLegiFragmenty = techContext.setLegiFragmenty;
 
   const rozdzielOprawe = () => {
-    console.log("intro")
+    console.log("intro");
+
+    let newFragmentyLeg = [];
+
+    for (let fragmentTech of fragmentyTech) {
+      
+
+      for (let legaFregment of legiFragmenty) {
+        newFragmentyLeg.push({
+          ...legaFregment,
+          oprawa_id: fragmentTech.oprawa_id,
+          naklad: fragmentTech.naklad,
+        });
+      }
 
 
-  }
+    }
+
+    setLegiFragmenty(newFragmentyLeg);
+  };
 
   return [rozdzielOprawe];
 }

56dc79793630207385b8d1790d0a73d1547e1917
Maciek <siedem@onet.pl>
2025-04-29 19:47:13 +0200

    zapis

--- src/components/TechnologiaStage/components/header/HeaderTech.jsx
+++ src/components/TechnologiaStage/components/header/HeaderTech.jsx
@@ -30,7 +30,7 @@ export default function Header({}) {
         console.log("Elementy Tech: ", techContext.elementyTech);
         console.log("Fragmenty Tech: ", techContext.fragmentyTech);
         console.log("Oprawa Tech: ", techContext.oprawaTech);
-        // console.log("Procesy elementów: ", techContext.procesyElementowTech);
+        console.log("Procesy elementów: ", techContext.procesyElementowTech);
         console.log("Arkusze: ", techContext.arkusze);
         console.log("Legi: ", techContext.legi);
         console.log("Fragmenty leg tech: ", techContext.legiFragmenty);

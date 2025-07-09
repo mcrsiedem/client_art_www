2969d8d299dc895a33571e8d10ebdff881769220
Maciek <maciek@artdruk.eu>
2025-07-09 07:50:57 +0200

    Revert "zapis"
    This reverts commit 7fe0e804ce32f9c14c06c9261048a6bc3bb22b2c.

--- src/components/TechnologiaStage/components/elementy/RowArkusze.jsx
+++ src/components/TechnologiaStage/components/elementy/RowArkusze.jsx
@@ -181,9 +181,7 @@ export default function RowArkusze  ({ row,i })  {
                           
                             })
 
-
-       //--------------------------   
-       // Po zmianie kolejności arkuszy przepisuje indeksy                  
+       //--------------------------                     
                             let new_legi = [...legi]
                             let s =0;
                             for( let ark of new_arkusze.filter(x => x.element_id ==id_element_arkusz_drag )){
@@ -212,7 +210,7 @@ export default function RowArkusze  ({ row,i })  {
         //-----------------------------------------                 
 
 
-
+                          //przenumerowanie indeksow leg wzgledem arkuszy
 
                                     setLegi(
                                       new_legi
@@ -235,10 +233,6 @@ export default function RowArkusze  ({ row,i })  {
                                       })
                                     );
                             // .sort((a, b) => a.oprawa_id - b.oprawa_id)
-
-
-
-                            // ponumerować jeszcze fragmenty
 
  setArkusze(new_arkusze )
 

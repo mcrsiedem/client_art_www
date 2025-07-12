b29e5aac3bd040b923f656d1d537ca0f24d2aae7
Maciek <siedem@onet.pl>
2025-07-12 21:55:51 +0200

    dziala

--- src/pages/Zamowienia/ModalInsert/Parametry/Parametry.jsx
+++ src/pages/Zamowienia/ModalInsert/Parametry/Parametry.jsx
@@ -1,17 +1,6 @@
 import style from "./Parametry.module.css";
-import { useContext} from "react";
 import { _firma, _produkty, _klient, _zestawy, _elementy, _opiekun, _status_dokumentu,_stan_dokumentu,_vat,_waluta,_rodzaj,_fsc, _etapy_produkcji, reg_txt } from "utils/initialvalue";
-import addIcon2 from "../../../../assets/addIcon2.svg";
-import { ModalInsertContext } from "context/ModalInsertContext";
-import { AppContext } from "context/AppContext";
-import { goInputValidation } from "actions/goInputValidation";
-import { useHistoria } from "hooks/useHistoria";
-import DecodeToken from "pages/Login/DecodeToken";
 
-import axios from "axios";
-
-import { IP } from "utils/Host";
-import { today } from "actions/today";
 export default function Parametery({showTabs,setShowTabs}) {
 
 
@@ -28,14 +17,6 @@ export default function Parametery({showTabs,setShowTabs}) {
   );
 }
 
-function Row({children,style}) {
-  
-    return (
-      <div className={style}>
-        {children}
-      </div>
-    );
-  }
 
 function PARAMETRY_BTN({showTabs,setShowTabs}) {
   

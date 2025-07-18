30db9f898ff9c348db3039358aceef1516c68da9
Maciek <maciek@artdruk.eu>
2025-07-18 17:17:52 +0200

    zapis

--- src/pages/ProcesyView/ProcesViewRow.jsx
+++ src/pages/ProcesyView/ProcesViewRow.jsx
@@ -37,7 +37,7 @@ import { useAccess } from "hooks/useAccess";
 import { getZamowieniaInfo } from "actions/getZamowieniaInfo";
 import { getZamowieniaInfoGrupy } from "actions/getZamowieniaInfoGrupy";
 import { druk_alert } from "actions/druk_alert";
-import { selectColor } from "actions/selectColor";
+// import { selectColor } from "actions/selectColor";
 
 
 
@@ -57,7 +57,48 @@ export default function ProcesViewRow({ grup,unlockTable, setUnlockTable,i }) {
         const [expand, setExpand] = useState(false);
         const [wolno] = useAccess(false);
 
+function selectColor (etapPlikow,status){
 
+            //druk
+            if(grup.proces_nazwa_id ==1){
+if (grup.select==true) return style.procesRow_select
+
+                          if (status==4 ) return style.procesRow_tr_DRUK
+            // if (status==2) return style.procesRow_tr_RIP
+    if (etapPlikow==1 && selectedProces==1) return style.procesRow_tr
+    if (etapPlikow==2 && selectedProces==1) return style.procesRow_tr
+    if (etapPlikow==3 && selectedProces==1) return style.procesRow_tr
+    if (etapPlikow==4 && selectedProces==1) return style.procesRow_tr_AKCEPT
+    if (etapPlikow==5 && selectedProces==1) return style.procesRow_tr_AKCEPT
+    if (etapPlikow==6 && selectedProces==1) return style.procesRow_tr_RIP
+    if (etapPlikow==7 && selectedProces==1) return style.procesRow_tr_ZAS
+       if (etapPlikow==8 && selectedProces==1 && status ==4) return style.procesRow_tr_DRUK
+    if (etapPlikow==8 && selectedProces==1) return style.procesRow_tr_RIP
+ 
+     return style.procesRow_tr
+            }
+
+            //wszystko poza drukiem
+            if(grup.proces_nazwa_id !=1){
+                          if (status==4 ) return style.procesRow_tr_DRUK
+            if (status==2) return style.procesRow_tr_RIP
+            if (status==3) return style.procesRow_tr_RIP
+    if (etapPlikow==1 && selectedProces==1) return style.procesRow_tr
+    if (etapPlikow==2 && selectedProces==1) return style.procesRow_tr
+    if (etapPlikow==3 && selectedProces==1) return style.procesRow_tr
+    if (etapPlikow==4 && selectedProces==1) return style.procesRow_tr_AKCEPT
+    if (etapPlikow==5 && selectedProces==1) return style.procesRow_tr_AKCEPT
+    if (etapPlikow==6 && selectedProces==1) return style.procesRow_tr_RIP
+    if (etapPlikow==7 && selectedProces==1) return style.procesRow_tr_ZAS
+       if (etapPlikow==8 && selectedProces==1 && status ==4) return style.procesRow_tr_DRUK
+    if (etapPlikow==8 && selectedProces==1) return style.procesRow_tr_RIP
+ 
+if (grup.select) return style.procesRow_select
+
+     return style.procesRow_tr
+            }
+
+  }
 
 
   return (

4957c7a33f70c03dd3127b0ed493eb6765c66065
Maciek <maciek@artdruk.eu>
2025-08-21 12:31:14 +0200

    zapis

--- src/pages/OprawaView/TechnologiaDetails/components/ElementRow.jsx
+++ src/pages/OprawaView/TechnologiaDetails/components/ElementRow.jsx
@@ -1,6 +1,9 @@
 import React, {useContext, } from "react";
 import style from "./ElementRow.module.css";
 import { TechnologyContext } from "context/TechnologyContext";
+import { getNameStatus } from "actions/getNameStatus";
+
+import { AppContext } from "context/AppContext";
 
 export default function ElementRow({ element,i }) {
   const techContext = useContext(TechnologyContext);
@@ -9,41 +12,53 @@ export default function ElementRow({ element,i }) {
   const elementyTech = techContext.elementyTech;
   const procesyElementowTech = techContext.procesyElementowTech;
 
+
+
   return (
     <div key={i} className={style.elementRow}>
-    
-
-      <div><p>{element.typ_nazwa}</p> </div>
-      <div className={style.row}>
-
+      <p className={style.title_nazwa} >{element.typ_nazwa}</p> 
        {procesyElementowTech
        .filter(x=> x.element_id == element.id)
                     .map((el, i) => {
-                      return ( <p>{el.nazwa}</p>)
+                      return ( <Row el={el}/>)
                       }
                   )}
-
-
- 
-      </div>
-
-
     </div>
   );
 }
 
 
-function Row({ elem }) {
+function Row({ el }) {
   const techContext = useContext(TechnologyContext);
+    const contextApp = useContext(AppContext);
+  
   const setGrupyOprawaAll = techContext.setGrupyOprawaAll;
   const grupyOprawaAll = techContext.grupyOprawaAll;
   const elementyTech = techContext.elementyTech;
   const procesyElementowTech = techContext.procesyElementowTech;
+  const _status_wykonania = contextApp._status_wykonania
+  const ifNull = (val) =>{
+
+if(val == null){
+  return ''
+}else return val
+   
+  }
+
+
+  const ifNullStatus = (val) =>{
+
+if(val == null){
+  return ''
+}else return getNameStatus(val, _status_wykonania)
+   
+  }
 
   return (
     <div className={style.row}>
 
-
+    <p>{el.nazwa + " "+ ifNull(el.rodzaj) + " "+ ifNull(el.typ)+ " "+ ifNull(el.wykonczenie)}</p>
+    <p>{ifNullStatus(el.status)}</p>
 
 
 

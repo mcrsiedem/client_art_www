223a4853c3a2e1caf22194ce36c1b08479f52d15
Maciek <maciek@artdruk.eu>
2025-08-21 11:43:56 +0200

    zapis

--- src/actions/wagaArkuszy.js
+++ src/actions/wagaArkuszy.js
@@ -1,5 +1,5 @@
 
 export  function wagaArkuszy (arkusz_szerokosc,arkusz_wysokosc,gramatura,przeloty) {
-          let waga = parseInt(arkusz_szerokosc)/1000 * parseInt(arkusz_wysokosc)/1000 * parseInt(gramatura) * parseInt(przeloty)
+          let waga = parseInt(arkusz_szerokosc)/1000 * parseInt(arkusz_wysokosc)/1000 * parseInt(gramatura)/1000 * parseInt(przeloty)
           return Math.ceil(waga)
         }

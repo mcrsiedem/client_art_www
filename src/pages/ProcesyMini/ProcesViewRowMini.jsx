5df182026c448519f7a5b3e5ff16016518a21492
Maciek <siedem@onet.pl>
2025-09-04 21:30:50 +0200

    zapis

--- src/pages/ProcesyMini/ProcesViewRowMini.jsx
+++ src/pages/ProcesyMini/ProcesViewRowMini.jsx
@@ -46,7 +46,7 @@ export default function ProcesViewRowMini({ grup,unlockTable, setUnlockTable }) 
         const [expand, setExpand] = useState(false);
           const selectColor = (etapPlikow,status) =>{
             if (status==2 ) return style.procesRow_tr_RIP
-            if (status==3 ) return style.procesRow_tr_RIP
+            if (status==3 ) return style.procesRow_tr_trakcie
             if (status==4 ) return style.procesRow_tr_DRUK
 
      return style.procesRow_tr
@@ -316,17 +316,11 @@ function Status({grup}) {
   const [sumujGrupe,statusGrupyProcesView,statusGrupyTechnologia,statusGrupyProcesViewPrzerwa,statusGrupyTechnologia_OPRAWA,statusGrupyTechnologia_OPRAWA_PROCESY] = useGrupyWykonan()
  const [add,dodajDoZamowienia] = useHistoria()
             const selectColor = (etap,status) =>{
-    // if (status==4) return style.select_DRUK
-    // if (etap==1) return style.select
-    // if (etap==2) return style.select
-    // if (etap==3) return style.select
-    // if (etap==4) return style.select_AKCEPT
-    // if (etap==5) return style.select_AKCEPT
-    // if (etap==6) return style.select_RIP
+
     if (status==2) return style.select_RIP
-    if (status==3) return style.select_RIP
+    if (status==3) return style.select_trakcie
     if (status==4) return style.select_DRUK
-      // if (etap==8) return style.select_DRUK
+ 
      return style.select
   }
   return (
--- src/pages/ProcesyMini/oprawa/RowMiniOprawa.jsx
+++ src/pages/ProcesyMini/oprawa/RowMiniOprawa.jsx
@@ -321,17 +321,11 @@ function Status({grup}) {
   const [sumujGrupe,statusGrupyProcesView,statusGrupyTechnologia,statusGrupyProcesViewPrzerwa,statusGrupyTechnologia_OPRAWA,statusGrupyTechnologia_OPRAWA_PROCESY] = useGrupyWykonan()
  const [add,dodajDoZamowienia] = useHistoria()
             const selectColor = (etap,status) =>{
-    // if (status==4) return style.select_DRUK
-    // if (etap==1) return style.select
-    // if (etap==2) return style.select
-    // if (etap==3) return style.select
-    // if (etap==4) return style.select_AKCEPT
-    // if (etap==5) return style.select_AKCEPT
-    // if (etap==6) return style.select_RIP
+
     if (status==2) return style.select_RIP
     if (status==3) return style.select_trakcie
     if (status==4) return style.select_DRUK
-      // if (etap==8) return style.select_DRUK
+ 
      return style.select
   }
   return (

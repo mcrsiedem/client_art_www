c82ebf73acf51925e2c3456ea24b778c19596f1d
Maciek <siedem@onet.pl>
2025-10-16 22:16:51 +0200

    zapis

--- src/pages/Panel/Panel.jsx
+++ src/pages/Panel/Panel.jsx
@@ -8,6 +8,7 @@ import { getNadkomplety } from "actions/getNadkomplety";
 import { getClients } from "actions/getClients";
 import PanelMini from "./Mini/PanelMini";
 import PanelDesktop from "./Desktop/PanelDesktop";
+import { getCurrentBuildHash } from "actions/getCurrentBuildHash";
 
 
 export default function Panel({ user, setUser }) {
@@ -17,10 +18,38 @@ export default function Panel({ user, setUser }) {
   const setNadkomplety = appcontext.setNadkomplety;
   const setClients = appcontext.setClients;
   const setClientsWyszukiwarka = appcontext.setClientsWyszukiwarka;
-
   useEffect(() => {
+  const currentHash = getCurrentBuildHash(); 
+
     getNadkomplety(setNadkomplety)
     getClients(setClients,setClientsWyszukiwarka )
+
+
+
+    // // 1. Sprawdź, czy udało się znaleźć własny hash
+    // if (!currentHash) {
+    //     console.error("Nie udało się zidentyfikować aktualnego hasha.");
+    //     return;
+    // }
+
+    // // 2. Wyślij zapytanie do API po oczekiwany hash
+    // axios.get('/api/check-version')
+    //   .then(response => {
+    //     const expectedHash = response.data.expectedHash;
+        
+    //     // 3. PORÓWNANIE
+    //     if (currentHash !== expectedHash) {
+    //       console.log(`Wersja: ${currentHash}. Oczekiwana: ${expectedHash}. Wymuszanie przeładowania...`);
+    //       window.location.reload(true); // Twarde przeładowanie
+    //     } else {
+    //       console.log(`Aplikacja jest aktualna (Hash: ${currentHash})`);
+    //     }
+    //   })
+    //   .catch(error => console.error('Błąd sprawdzania wersji', error));
+ 
+
+
+
   }, []);
 
   const logout = () => {

dde718944640d87cc32fa459d20b6d7a5652e389
Maciek <maciek@artdruk.eu>
2026-02-04 15:41:45 +0100

    zapis

--- src/pages/Panel/Desktop/Kalkulator/Kalkulator.jsx
+++ src/pages/Panel/Desktop/Kalkulator/Kalkulator.jsx
@@ -14,7 +14,7 @@ const Kalkulator = () => {
   
   const [sections, setSections] = useState([
     { id: 1, pages: 80, thickness: 0.0648, label: 'Środek', papier_id: 10 },
-    { id: 1, pages: 4, thickness: 0.20, label: 'Okładka', papier_id: 83 }
+    { id: 2, pages: 4, thickness: 0.20, label: 'Okładka', papier_id: 83 }
   ]);
   
   const [totalThickness, setTotalThickness] = useState(0);

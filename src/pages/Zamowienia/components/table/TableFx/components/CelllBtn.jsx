40ab32a42d5b8ec6be85b175d7b78dcad33b880a
Maciek <maciek@artdruk.eu>
2026-03-23 12:47:06 +0100

    zapis

--- src/pages/Zamowienia/components/table/TableFx/components/CelllBtn.jsx
+++ src/pages/Zamowienia/components/table/TableFx/components/CelllBtn.jsx
@@ -13,8 +13,12 @@ import DecodeToken from "pages/Login/DecodeToken";
 export default function  CellBtn({ row }) {
 
   if(row.open_stan == 1 ) return <BtnLock row={row}/>
+  if( row.technologia_stan == 1 )  return <BtnFileYellow row={row}/>
+  if( row.stan>2  && row.technologia_id == null)  return <BtnCirclePlus row={row}/>
   if( row.stan>2  && row.technologia_id == null)  return <BtnCirclePlus row={row}/>
 
+
+  // return row.stan>2  &&<BtnFile row={row}/>
   return row.stan>2  &&<BtnFile row={row}/>
 
 }
@@ -113,6 +117,44 @@ function BtnFile({
             alt="Procesy"
           >
 <FileText size={16} style={{color: '#94a3b8',border:'none'}} />
+
+          </button>
+         
+     
+      
+    );
+  
+}
+
+
+function BtnFileYellow({
+  row,
+}) {
+  const techContext = useContext(TechnologyContext);
+  const contextApp = useContext(AppContext);
+  const fechparametryTechnologii = techContext.fechparametryTechnologii;
+  const setShowProcesy = techContext.setShowProcesy;
+  
+
+
+
+    return (
+    
+      
+
+       <button style={{color: '#db850d',border:'none',background:'transparent'}}
+
+            onClick={() => {
+                           if(DecodeToken(sessionStorage.getItem("token")).technologie_wszystkie == 1){
+                                    contextApp.setIsLoading(true);
+                                            fechparametryTechnologii(row.id, row.technologia_id);
+                           }
+
+            
+            }}
+            alt="Procesy"
+          >
+<FileText size={16} style={{color: '#db850d',border:'none'}} />
 
           </button>
          

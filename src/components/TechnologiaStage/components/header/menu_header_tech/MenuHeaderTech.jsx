a160b741537e2561fdc9e66e3ad14c9692554980
Maciek <maciek@artdruk.eu>
2026-04-09 12:52:19 +0200

    zapis

--- src/components/TechnologiaStage/components/header/menu_header_tech/MenuHeaderTech.jsx
+++ src/components/TechnologiaStage/components/header/menu_header_tech/MenuHeaderTech.jsx
@@ -18,31 +18,6 @@ export default function MenuHeaderTech({ showMenu, setShowMenu }) {
   if (showMenu) {
     return (
       <div className={style.menu_legi}>
-         {/* <DodajNowyElement row={row}  /> */}
-         {/* <DodajArkusz row={row}  />
-        <Ponumeruj row={row} />
-
-                <button
-                  className={style.menu_legi_btn}
-                  onClick={() => {
-
-      createProcesyFromArkuszONE()
-        setShowProcesy(true)
-
-                    setElementyTech(elementyTech.map((t) => {
-                      return {...t,
-                        showMenu: false}
-                    }));
-         
-                  }}
-                >
-                Dodaj procesy
-                </button>
-
-        <KopiujFormatPapieru row={row} />
-        <KopiujNadkomplet row={row} /> */}
-
-        
         <Zamowienie  setShowMenu={setShowMenu}/>
         <Anuluj  setShowMenu={setShowMenu}/>
 
@@ -51,132 +26,8 @@ export default function MenuHeaderTech({ showMenu, setShowMenu }) {
   }
 }
 
-const Ponumeruj = ({ row}) =>{
-  const techContext = useContext(TechnologyContext);
-
-  const elementyTech = techContext.elementyTech;
-  const setElementyTech = techContext.setElementyTech;
-  const setArkusze = techContext.setArkusze;
-  const arkusze = techContext.arkusze;
-  const legi = techContext.legi;
-  const setLegi = techContext.setLegi;
-  return(
-    <button
-    className={style.menu_legi_btn}
-    onClick={() => {
-
-      // setNumerArkusza2(row)
-      ponumerArkusze(row,setArkusze,arkusze,legi,setLegi)
-      setElementyTech(elementyTech.map((t) => {
-        return {...t,
-          showMenu: false}
-      }));
-
-    }}
-  >
-    Ponumeruj
-  </button>
-  )
-}
-
-const KopiujFormatPapieru = ({ row}) =>{
-  const techContext = useContext(TechnologyContext);
-  const setArkusze = techContext.setArkusze;
-  const arkusze = techContext.arkusze;
-  const setElementyTech = techContext.setElementyTech;
-  const elementyTech = techContext.elementyTech;
-
-  return(
-    <button
-    className={style.menu_legi_btn}
-    onClick={() => {
-
-// setArkusze(
-//   arkusze.map((arkusz) => {
-//     if (arkusz.element_id === row.id) {
-//       return {
-//         ...arkusz,
-//         arkusz_szerokosc: row.arkusz_szerokosc,
-//         arkusz_wysokosc: row.arkusz_wysokosc,
-//         update: true
-
-//       };
-//     } else {
-//       return arkusz;
-//     }
-//   })
-// );
-
-setArkusze(prevArkusze => 
-  prevArkusze.map((arkusz) => {
-    if (arkusz.element_id === row.id) {
-      return {
-        ...arkusz,
-        arkusz_szerokosc: row.arkusz_szerokosc,
-        arkusz_wysokosc: row.arkusz_wysokosc,
-        update: true
-      };
-    }
-    return arkusz;
-  })
-);
 
 
-
-// setElementyTech(elementyTech.map((t) => {
-//   return {...t,
-//     showMenu: false}
-// }));
-
-
-setElementyTech(currentTech => 
-    currentTech.map(t => ({ ...t, showMenu: false }))
-  );
-
-    }}
-  >
-    Rozdaj format papieru ark.
-  </button>
-  )
-}
-
-const KopiujNadkomplet = ({ row}) =>{
-  const techContext = useContext(TechnologyContext);
-  const setArkusze = techContext.setArkusze;
-  const arkusze = techContext.arkusze;
-  const setElementyTech = techContext.setElementyTech;
-  const elementyTech = techContext.elementyTech;
-
-  return(
-    <button
-    className={style.menu_legi_btn}
-    onClick={() => {
-
-setArkusze(
-  arkusze.map((arkusz) => {
-    if (arkusz.element_id === row.id) {
-      return {
-        ...arkusz,
-        nadkomplet: row.nadkomplet,
-        update: true
-
-      };
-    } else {
-      return arkusz;
-    }
-  })
-);
-setElementyTech(elementyTech.map((t) => {
-  return {...t,
-    showMenu: false}
-}));
-
-    }}
-  >
-    Rozdaj nadkomplet
-  </button>
-  )
-}
 
 
 const Zamowienie = ({setShowMenu} ) =>{
@@ -219,18 +70,5 @@ const Anuluj = ({setShowMenu} ) =>{
 
 
 
-const DodajArkusz= ({ row }) =>{
-  const [dodajArkusz] = useArkuszeOne()
-  return(
-    <button
-    className={style.menu_legi_btn}
-    onClick={() => {
-         dodajArkusz(row)
-    }}
-  >
-    Nowy arkusz
-  </button>
-  )
-}
 
 

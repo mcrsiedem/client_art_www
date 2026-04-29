1afd6d18fe9e40a6c3b7d176bcc20b00342a26b8
Maciek <siedem@onet.pl>
2026-04-29 21:15:59 +0200

    zapis

--- src/pages/Statystyki/Wykres/Wykres.jsx
+++ src/pages/Statystyki/Wykres/Wykres.jsx
@@ -11,8 +11,8 @@ const Wykres = () => {
 
   const LIMIT_MATRIX = {
     all: { daily: 40000, weekly: 2000000, monthly: 8000000 },
-    main: { daily: 30000, weekly: 2000000, monthly: 8000000 },
-    finishing: { daily: 5000, weekly: 25000, monthly: 80000 }
+    main: { daily: 300000, weekly: 2000000, monthly: 8000000 },
+    finishing: { daily: 50000, weekly: 25000, monthly: 80000 }
   };
 
   const allConfig = [

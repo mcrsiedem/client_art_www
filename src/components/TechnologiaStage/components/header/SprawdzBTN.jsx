7d4107890f34c97261d2f0c028c07b3c1c070b75
Maciek <siedem@onet.pl>
2026-05-02 21:12:48 +0200

    zapis

--- src/components/TechnologiaStage/components/header/SprawdzBTN.jsx
+++ src/components/TechnologiaStage/components/header/SprawdzBTN.jsx
@@ -3,64 +3,30 @@ import { useContext } from "react";
 import { TechnologyContext } from "context/TechnologyContext";
 import DecodeToken from "pages/Login/DecodeToken";
 
-
 //----
-export default function SprawdzBTN () {
+export default function SprawdzBTN() {
   const techContext = useContext(TechnologyContext);
   const procesyElementowTech = techContext.procesyElementowTech;
   const grupaWykonan = techContext.grupaWykonan;
   const wykonania = techContext.wykonania;
   const setSaveButtonDisabled = techContext.setSaveButtonDisabled;
- if (DecodeToken(sessionStorage.getItem("token")).technologia_zapis == 1) {
-  return (
-    <button
-      // disabled={isSaveButtonDisabled}
-      className={ style.btn}
-      onClick={() => {
-
-            if (DecodeToken(sessionStorage.getItem("token")).technologia_zapis == 1) {
-
-    //   const promiseA = new Promise((resolve, reject) => {
-
-    //     if(procesyElementowTech.length == grupaWykonan.length){
-    //     resolve(777);
-    //     } else{
-    //       reject("Dodaj procesy")
-    //     }
-
-          
-    //     })
-    //     promiseA.then(res => setSaveButtonDisabled(false)  ).catch(function(rej) {
-    //   alert(rej)
-    //   console.log(rej);
-    // });
-setSaveButtonDisabled(false)
-
-            }
-
-           
-    
-    //       const promiseB = new Promise((resolve, reject) => {
-
-    //     if(wykonania.map(x => x.proces_id).includes(procesyElementowTech.map(x => x.id))){
-    //     resolve(777);
-    //     } else{
-    //       reject("Nie za każdego procesu wygenerowały się wykonania. ")
-    //     }
-
-    //     })
-    //     promiseB.then(res => setSaveButtonDisabled(false)  ).catch(function(rej) {
-    //   alert(rej)
-    //   console.log(rej);
-    // });
-
-
-
-        
-      }}
-    >
-      Sprawdź 
-    </button>
-  );
+  if (
+    DecodeToken(sessionStorage.getItem("token")).technologia_zapis == 1 &&
+    grupaWykonan.length > 0
+  ) {
+    return (
+      <button
+        className={style.btn}
+        onClick={() => {
+          if (
+            DecodeToken(sessionStorage.getItem("token")).technologia_zapis == 1
+          ) {
+            setSaveButtonDisabled(false);
+          }
+        }}
+      >
+        Sprawdź
+      </button>
+    );
+  }
 }
-};
